06903e2450ae5c0bb664ade37a7d62312b71cb71
hyune-c <choi8608@gmail.com>
2022-11-06 05:16:01 +0900

    🔧 project init

---
+++ src/main/java/com/hyunec/cosmicbaseballinit/CosmicBaseballInitApplication.java
@@ -0,0 +1,13 @@
+package com.hyunec.cosmicbaseballinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmicBaseballInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmicBaseballInitApplication.class, args);
+    }
+
+}
