95df3a9f8f419ac868d5320e7907b8a86bf0c54b
Shin seongsu <tlstjdtn321@naver.com>
2022-12-07 03:41:01 +0900

    :sparkles: 배팅 관련 Controller 구현

---
+++ src/main/java/com/hyunec/cosmicbaseballinit/hitter/ui/BattingController.java
@@ -0,0 +1,25 @@
+package com.hyunec.cosmicbaseballinit.hitter.ui;
+
+import com.hyunec.cosmicbaseballinit.hitter.application.BattingService;
+import com.hyunec.cosmicbaseballinit.hitter.domain.Batting;
+import com.hyunec.cosmicbaseballinit.hitter.domain.BattingResult;
+import com.hyunec.cosmicbaseballinit.hitter.domain.RandomGenerate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BattingController {
+
+    private final BattingService battingService;
+    private final RandomGenerate randomGenerate;
+
+    @GetMapping("/game/batting")
+    public BattingResult batting() {
+        Batting batting = Batting.generate(randomGenerate);
+        BattingResult battingResult = battingService.swing(batting, randomGenerate);
+        return battingResult;
+    }
+
+}
