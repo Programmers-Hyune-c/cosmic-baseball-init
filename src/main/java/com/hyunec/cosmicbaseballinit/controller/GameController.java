f863d4fecc9a4b35e0120c8c09de76349b31f66b
jyg <yonggyujeong10@gmail.com>
2022-12-06 13:19:35 +0900

    add GameController and test (1 method : game start)

--- src/main/java/com/hyunec/cosmicbaseballinit/controller/GameController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/controller/GameController.java
@@ -1,6 +1,8 @@
 package com.hyunec.cosmicbaseballinit.controller;
 
+import com.hyunec.cosmicbaseballinit.service.OneWeekGameService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,11 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class GameController {
 
-    public GameController() {
+    @Autowired
+    private OneWeekGameService gameService;
+
+    public GameController(OneWeekGameService gameService) {
+        this.gameService = gameService;
     }
 
     @GetMapping("/gameStart")
-    public void gameStart(){
-
+    public String gameStart(){
+        String gameResult = gameService.gameStarted();
+        log.info("----------gameResult : " + gameResult + "----------");
+        return gameResult;
     }
 }
---
+++ src/test/java/com/hyunec/cosmicbaseballinit/controller/GameControllerTest.java
@@ -0,0 +1,21 @@
+package com.hyunec.cosmicbaseballinit.controller;
+
+import com.hyunec.cosmicbaseballinit.service.OneWeekGameService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameControllerTest {
+
+
+    GameController gameController = new GameController(new OneWeekGameService());
+
+    @Test
+    void gameStartTest(){
+        String result = gameController.gameStart();
+
+        Assertions.assertThat(result).isEqualTo("DRAW");
+    }
+}
