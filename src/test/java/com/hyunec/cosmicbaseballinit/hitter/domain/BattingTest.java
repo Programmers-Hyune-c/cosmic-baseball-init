d0af6c5b9019b3e711158577b74225f8b28bef3e
Shin seongsu <tlstjdtn321@naver.com>
2022-12-07 03:43:03 +0900

    :white_check_mark: 배팅 테스트

---
+++ src/test/java/com/hyunec/cosmicbaseballinit/hitter/domain/BattingTest.java
@@ -0,0 +1,45 @@
+package com.hyunec.cosmicbaseballinit.hitter.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class BattingTest {
+
+    RandomGenerate randomGenerate;
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void generateTest(int ordinal, Batting result) {
+        randomGenerate = new RandomGenerate() {
+            @Override
+            public int generate() {
+                return ordinal;
+            }
+
+            @Override
+            public int percentageGenerate() {
+                return 0;
+            }
+        };
+
+        Batting actual = Batting.generate(randomGenerate);
+
+        assertThat(actual).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+            arguments(0, Batting.STRIKE),
+                arguments(1, Batting.BALL),
+                arguments(2, Batting.HIT)
+        );
+    }
+
+}
