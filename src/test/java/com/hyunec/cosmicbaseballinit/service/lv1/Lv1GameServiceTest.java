69b9b71bdcb0d3689ec03971217956534f6c57fe
jyg <yonggyujeong10@gmail.com>
2022-12-10 22:59:20 +0900

    - 서비스명 변경, lv1 폴더 추가

--- src/test/java/com/hyunec/cosmicbaseballinit/service/lv1/Lv1GameServiceTest.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/service/lv1/Lv1GameServiceTest.java
@@ -1,15 +1,15 @@
-package com.hyunec.cosmicbaseballinit.service;
+package com.hyunec.cosmicbaseballinit.service.lv1;
 
+import com.hyunec.cosmicbaseballinit.service.lv1.Lv1GameService;
 import com.hyunec.cosmicbaseballinit.vo.GameResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-class OneLevelGameTest {
+class Lv1GameServiceTest {
 
-    OneLevelGameService oneLevelGameService = new OneLevelGameService();
-
+    Lv1GameService lv1GameService = new Lv1GameService();
 
     @Test
     @DisplayName("게임 끝남 메서드가 팀 스코어에 따라 올바른 결과 값을 반환하는지 테스트")
@@ -17,7 +17,7 @@ class OneLevelGameTest {
         Integer myTeamScore = 1;
         Integer otherTeamScore = 4;
 
-        GameResult gameResult = oneLevelGameService.gameFinished(myTeamScore, otherTeamScore);
+        GameResult gameResult = lv1GameService.gameFinished(myTeamScore, otherTeamScore);
 
         assertThat(gameResult).isEqualTo(GameResult.LOSE);
 
