cd5171a5f44f6c455efe82a0ca5852e75cbb9286
Shin seongsu <tlstjdtn321@naver.com>
2022-12-10 19:06:31 +0900

    :white_check_mark: oridinal 보다는 넘버를 상수로 사용하여 사용하기

--- src/main/java/com/hyunec/cosmicbaseballinit/hitter/domain/Batting.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/hitter/domain/Batting.java
@@ -1,10 +1,14 @@
 package com.hyunec.cosmicbaseballinit.hitter.domain;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public enum Batting {
-    STRIKE,
-    BALL,
-    HIT;
+    STRIKE(0),
+    BALL(1),
+    HIT(2);
+
+    private final int orderNumber;
 }
