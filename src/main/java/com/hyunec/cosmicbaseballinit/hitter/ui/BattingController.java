fd981396d6901319ee9ace1cbf237c6c3bbd6298
Shin seongsu <tlstjdtn321@naver.com>
2022-12-10 19:09:18 +0900

    :white_check_mark: Presentation layer 수정

--- src/main/java/com/hyunec/cosmicbaseballinit/hitter/ui/BattingController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/hitter/ui/BattingController.java
@@ -2,7 +2,9 @@ package com.hyunec.cosmicbaseballinit.hitter.ui;
 
 import com.hyunec.cosmicbaseballinit.hitter.application.BattingService;
 import com.hyunec.cosmicbaseballinit.hitter.domain.Batting;
-import com.hyunec.cosmicbaseballinit.hitter.domain.RandomGenerate;
+import com.hyunec.cosmicbaseballinit.hitter.domain.Battings;
+import com.hyunec.cosmicbaseballinit.hitter.domain.BattingGenerator;
+import com.hyunec.cosmicbaseballinit.hitter.ui.component.PlateAppearance;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class BattingController {
 
     private final BattingService battingService;
-    private final RandomGenerate randomGenerate;
+    private final BattingGenerator battingGenerator;
+    private final PlateAppearance plateAppearance;
 
     @GetMapping("/game/batting")
     public String batting() {
-        Batting batting = randomGenerate.generate();
-        return battingService.swing(batting);
+        final Batting batting = battingGenerator.generate();
+        final Battings battings = battingService.swing(batting);
+        final String battingResult = plateAppearance.result(battings);
+
+        battingService.clearBattings(battingResult);
+
+        return battingResult;
     }
 }
