05a618eaabd7e331f210a2b99e7a58599b9f5e18
jyg <yonggyujeong10@gmail.com>
2022-12-14 14:44:23 +0900

    Controller 메서드 수정(게임 세팅, 히팅, 결과반환)

--- src/main/java/com/hyunec/cosmicbaseballinit/controller/GameController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/controller/GameController.java
@@ -1,6 +1,7 @@
 package com.hyunec.cosmicbaseballinit.controller;
 
 import com.hyunec.cosmicbaseballinit.service.lv1.Lv1GameService;
+import com.hyunec.cosmicbaseballinit.service.lv1.Lv1HitterGameService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class GameController {
 
-    private final Lv1GameService gameService;
+    private final Lv1HitterGameService gameService;
 
-    @GetMapping("/game/start")
-    public void gameStart() {
+    @GetMapping("/game/setting")
+    public String gameSetting(){
+        gameService.setHitGameProbability();
+        return "Probability setting finished";
+    }
+
+    @GetMapping("/game/hitting")
+    public String hitting(){
         try {
-            gameService.gameStart();
-        } catch(Exception e) {
-            e.printStackTrace();
+            // TODO PitchResult에 따라 GO/OUt 판정하고, list 초기화 하도록 수정
+            return gameService.hitting();
+        } catch (Exception e) {
+            return e.getMessage();
         }
+    }
+
+    // TODO 현재 STRIKE, BALL, OUT 갯수 리스트 반환
+    @GetMapping("/game/hittingResults")
+    public void gameResult(){
+
     }
 }
