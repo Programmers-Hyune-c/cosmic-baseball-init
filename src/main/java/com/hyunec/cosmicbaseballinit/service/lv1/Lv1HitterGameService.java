ec4485c1914ef6ce49775ba255f6714ef75a00e0
jyg <yonggyujeong10@gmail.com>
2022-12-14 14:38:33 +0900

    HitterGame 확률 세팅 함수, PitchResult 동일 확률 반환 함수

---
+++ src/main/java/com/hyunec/cosmicbaseballinit/service/lv1/Lv1HitterGameService.java
@@ -0,0 +1,27 @@
+package com.hyunec.cosmicbaseballinit.service.lv1;
+
+import com.hyunec.cosmicbaseballinit.vo.HitterResult;
+import com.hyunec.cosmicbaseballinit.vo.IntHolder;
+import com.hyunec.cosmicbaseballinit.vo.PitchResult;
+import com.hyunec.cosmicbaseballinit.vo.ProbabilityType;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Slf4j
+public class Lv1HitterGameService {
+    public final Map<PitchResult, Double> probabilityMap = new HashMap<>();
+    public final List<PitchResult> hittingResult = new ArrayList<>();
+
+    // 확률 세팅
+    public void setHitGameProbability(){
+        PitchResult.settingProbability(probabilityMap);
+    }
+
+
+}
--- src/main/java/com/hyunec/cosmicbaseballinit/vo/PitchResult.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/vo/PitchResult.java
@@ -2,12 +2,39 @@ package com.hyunec.cosmicbaseballinit.vo;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.Map;
 
 @Getter
 @RequiredArgsConstructor
+@Slf4j
 public enum PitchResult {
     STRIKE(),
     BALL(),
     HIT();
+
+    // 동일한 확률을 계산하는 함수
+    private static Double calculateSameProbability() {
+        Double entireNumber = 1.0;
+        Double numberOfHitterResult = (double) PitchResult.values().length;
+        return entireNumber / numberOfHitterResult;
+    }
+
+    // 확률 세팅 되었는지 체크
+    private static Boolean isProbabilitySetting(Map<PitchResult, Double> probabilityMap){
+        return probabilityMap.size() == PitchResult.values().length;
+    }
+
+    // 확률 세팅
+    public static void settingProbability(Map<PitchResult, Double> probabilityMap){
+        if(isProbabilitySetting(probabilityMap)){
+            return;
+        }
+        Double sameProbability = PitchResult.calculateSameProbability();
+        Arrays.stream(PitchResult.values()).forEach(pitchResult -> probabilityMap.put(pitchResult, sameProbability));
+    }
+
 }
