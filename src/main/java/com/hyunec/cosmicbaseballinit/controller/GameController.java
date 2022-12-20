95607fd22ab1069d6149de56d31a1aeb375423a8
jyg <yonggyujeong10@gmail.com>
2022-12-20 09:46:23 +0900

    Controller hitting 점수 반환 메서드

--- src/main/java/com/hyunec/cosmicbaseballinit/controller/GameController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/controller/GameController.java
@@ -1,10 +1,13 @@
 package com.hyunec.cosmicbaseballinit.controller;
 
 import com.hyunec.cosmicbaseballinit.service.lv1.Lv1HitterGameService;
+import com.hyunec.cosmicbaseballinit.vo.PitchResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +22,7 @@ public class GameController {
         return "Probability setting finished"; //TODO: 하드코딩된 문자열 반환 처리하기
     }
 
-    @GetMapping("/game/hitting") //TODO: try-catch 대신 ControllerAdvice
+    @GetMapping("/game/hitting") // TODO: try-catch 대신 ControllerAdvice
     public String hitting(){
         try {
             // TODO PitchResult에 따라 GO/OUt 판정하고, list 초기화 하도록 수정
@@ -30,8 +33,8 @@ public class GameController {
     }
 
     // TODO 현재 STRIKE, BALL, OUT 갯수 리스트 반환
-    @GetMapping("/game/hittingResults")
-    public void gameResult(){
-
+    @GetMapping("/game/hittingScore")
+    public Map<PitchResult, Integer> hittingScore(){
+        return gameService.getScores();
     }
 }
