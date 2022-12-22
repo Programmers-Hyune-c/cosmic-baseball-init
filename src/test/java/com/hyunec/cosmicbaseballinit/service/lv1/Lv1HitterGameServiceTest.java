264e1c1de9c4099950d7e2b581ec952e29d362dd
jyg <yonggyujeong10@gmail.com>
2022-12-22 12:49:14 +0900

    시나리오 테스트 두개로 분리

--- src/test/java/com/hyunec/cosmicbaseballinit/service/lv1/Lv1HitterGameServiceTest.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/service/lv1/Lv1HitterGameServiceTest.java
@@ -23,9 +23,9 @@ public class Lv1HitterGameServiceTest {
 
     String hitterResult;
 
-    @DisplayName("Lv1 일반야구 게임 세팅, 투구 결과에 따른 타자의 결과 반환")
+    @DisplayName("Lv1 일반야구 게임 세팅, Strike_out")
     @TestFactory
-    Stream<DynamicTest> lv1GeneralGameSettingToReturnHitterResult(){
+    Stream<DynamicTest> lv1GeneralGameSettingToStrikeOut(){
         return Stream.of(
                 dynamicTest("동일 확률로 PitchResult 반환하도록 게임 세팅", () -> {
                     // given
@@ -65,43 +65,55 @@ public class Lv1HitterGameServiceTest {
                     // then
                     hitterResult = (String) returnHittingResult.invoke(lv1HitterGameService, result3);
                     Assertions.assertThat(hitterResult).isEqualTo(HitterResult.STRIKE_OUT.name());
-                }),
-                dynamicTest("점수 초기화", () -> {
-                    //when
-                   if (lv1HitterGameService.isHitterGameEnd(hitterResult)) {
-                       lv1HitterGameService.initGameScore();
-                   }
-
-                   //then
-                    for (Integer score : lv1HitterGameService.getScores().values()) {
-                        assertThat(score).isEqualTo(0);
-                    }
-                }),
-                dynamicTest("PitchResult Ball 4개 일시, FOUR_BALL 반환", () ->{
-                    // given
-                    PitchResult result1 = PitchResult.BALL;
-                    PitchResult result2 = PitchResult.BALL;
-                    PitchResult result3 = PitchResult.BALL;
-                    PitchResult result4 = PitchResult.BALL;
-
-                    // when
-                    Method method1 = lv1HitterGameService.getClass().getDeclaredMethod("savePitchResult"
-                            , PitchResult.class);
-                    method1.setAccessible(true);
-                    Method returnHittingResult = lv1HitterGameService.getClass().getDeclaredMethod("returnHittingResult"
-                            , PitchResult.class);
-                    returnHittingResult.setAccessible(true);
-
-                    method1.invoke(lv1HitterGameService, result1);
-                    method1.invoke(lv1HitterGameService, result2);
-                    method1.invoke(lv1HitterGameService, result3);
-                    method1.invoke(lv1HitterGameService, result4);
-
-                    // then
-                    String hitterResult = (String) returnHittingResult.invoke(lv1HitterGameService, result4);
-                    Assertions.assertThat(hitterResult).isEqualTo(HitterResult.FOUR_BALL.name());
-                    lv1HitterGameService.initGameScore();
                 })
+        );
+    }
+
+    @DisplayName("Lv1 일반야구 게임 세팅, FourBall")
+    @TestFactory
+    Stream<DynamicTest> lv1GeneralGameSettingToFourBall(){
+        return Stream.of(
+            dynamicTest("동일 확률로 PitchResult 반환하도록 게임 세팅", () -> {
+                // given
+
+                // when
+                lv1HitterGameService.setHitGameProbability();
+                Double totalProbability = 0D;
+
+                // then
+                assertThat(lv1HitterGameService.getProbabilityMap().size())
+                        .isEqualTo(PitchResult.values().length);
+
+                for (PitchResult key : lv1HitterGameService.getProbabilityMap().keySet()) {
+                    totalProbability += lv1HitterGameService.getProbabilityMap().get(key);
+                }
+                assertThat(totalProbability).isEqualTo(1);
+            }),
+            dynamicTest("PitchResult Ball 4개 일시, FOUR_BALL 반환", () ->{
+                // given
+                PitchResult result1 = PitchResult.BALL;
+                PitchResult result2 = PitchResult.BALL;
+                PitchResult result3 = PitchResult.BALL;
+                PitchResult result4 = PitchResult.BALL;
+
+                // when
+                Method method1 = lv1HitterGameService.getClass().getDeclaredMethod("savePitchResult"
+                        , PitchResult.class);
+                method1.setAccessible(true);
+                Method returnHittingResult = lv1HitterGameService.getClass().getDeclaredMethod("returnHittingResult"
+                        , PitchResult.class);
+                returnHittingResult.setAccessible(true);
+
+                method1.invoke(lv1HitterGameService, result1);
+                method1.invoke(lv1HitterGameService, result2);
+                method1.invoke(lv1HitterGameService, result3);
+                method1.invoke(lv1HitterGameService, result4);
+
+                // then
+                String hitterResult = (String) returnHittingResult.invoke(lv1HitterGameService, result4);
+                Assertions.assertThat(hitterResult).isEqualTo(HitterResult.FOUR_BALL.name());
+                lv1HitterGameService.initGameScore();
+            })
         );
     }
 }
