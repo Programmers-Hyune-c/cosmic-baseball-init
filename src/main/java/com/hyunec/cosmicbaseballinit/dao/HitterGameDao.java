1abc6025ae426aa5a41d55d79a1b1e934fa56ee8
jyg <yonggyujeong10@gmail.com>
2023-01-07 23:45:25 +0900

    HitterGame에 타구게임 최종 결과(STRIKE_OUT, FOUR_BALL...)를 저장하는 hitterResult 생성

--- src/main/java/com/hyunec/cosmicbaseballinit/dao/HitterGameDao.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/dao/HitterGameDao.java
@@ -1,17 +1,23 @@
 package com.hyunec.cosmicbaseballinit.dao;
 
 import com.hyunec.cosmicbaseballinit.repository.HitterGameRepository;
+import com.hyunec.cosmicbaseballinit.vo.hitterGame.HitterResult;
 import com.hyunec.cosmicbaseballinit.vo.hitterGame.PitchResult;
+import lombok.Getter;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Getter
 @Repository
 public class HitterGameDao implements HitterGameRepository {
 
     // 투구 결과
-    public List<PitchResult> pitchResultList = new ArrayList<>();
+    private List<PitchResult> pitchResultList = new ArrayList<>();
+    // 타구 게임 결과
+    private Optional<HitterResult> hitterResult = Optional.empty();
 
     @Override
     public List<PitchResult> getPitchResultList() {
@@ -31,5 +37,17 @@ public class HitterGameDao implements HitterGameRepository {
     @Override
     public void init() {
         pitchResultList.clear();
+        hitterResult = Optional.empty();
+    }
+
+    @Override
+    public void saveHitterGameResult(HitterResult hitterResult) {
+        this.hitterResult = Optional.of(hitterResult);
+    }
+
+    // hitterResult 값이 존재하면 게임이 끝났다고 간주
+    @Override
+    public Boolean isHitterGameEnd() {
+        return (this.hitterResult.isPresent());
     }
 }
--- src/main/java/com/hyunec/cosmicbaseballinit/repository/HitterGameRepository.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/repository/HitterGameRepository.java
@@ -1,5 +1,6 @@
 package com.hyunec.cosmicbaseballinit.repository;
 
+import com.hyunec.cosmicbaseballinit.vo.hitterGame.HitterResult;
 import com.hyunec.cosmicbaseballinit.vo.hitterGame.PitchResult;
 
 import java.util.List;
@@ -10,4 +11,9 @@ public interface HitterGameRepository {
     void savePitchResult(PitchResult pitchResult);
     Integer getCountByPitchResult(PitchResult pitchResult);
     void init();
+    // 최종 hitterGame 결과 저장
+    void saveHitterGameResult(HitterResult hitterResult);
+
+    // 게임 끝났는지 확인
+    Boolean isHitterGameEnd();
 }
