dcc9859a4acb372629dbc2c7f91122a72c721af1
Hyune-c <choi8608@gmail.com>
2023-01-08 22:16:04 +0900

    ✅ 일반 야구 & 우주 야구 lv1 인수 테스트 샘플 추가

---
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/CosmicBaseballLv1Test.java
@@ -0,0 +1,25 @@
+package com.hyunec.cosmicbaseballinit.acceptancetest;
+
+import org.junit.jupiter.api.DisplayName;
+
+class CosmicBaseballLv1Test {
+    @DisplayName("strike 의 20% 는 bullseye_strike 입니다.")
+    void t1() {
+    }
+
+    @DisplayName("ball 의 20% 는 bullseye_ball 입니다.")
+    void t2() {
+    }
+
+    @DisplayName("hit 의 20% 는 homerun 입니다.")
+    void t3() {
+    }
+
+    @DisplayName("타격 결과가 bullseye_strike 이면 타석 결과는 out 됩니다.")
+    void t4() {
+    }
+
+    @DisplayName("타격 결과가 bullseye_ball 이면 타석 결과는 hit 됩니다.")
+    void t5() {
+    }
+}
---
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
@@ -0,0 +1,25 @@
+package com.hyunec.cosmicbaseballinit.acceptancetest;
+
+import org.junit.jupiter.api.DisplayName;
+
+class NormalBaseballLv1Test {
+    @DisplayName("strike, ball, hit 는 같은 확률 입니다.")
+    void t1() {
+    }
+
+    @DisplayName("3B 타석에서 타격 결과가 ball 이면 타석 결과는 four_ball 됩니다.")
+    void t2() {
+    }
+
+    @DisplayName("2S 타석에서 타격 결과가 strike 이면 타석 결과는 out 됩니다.")
+    void t3() {
+    }
+
+    @DisplayName("진행 중인 타석이 있는 상태에서 새로운 타석을 진행할 수 없습니다.")
+    void t4() {
+    }
+
+    @DisplayName("타석이 종료되면 초기화하여 새로 진행할 수 있습니다.")
+    void t5() {
+    }
+}
