d3a7296e019d02ce7ef8563a86da8f5e79638499
jyg <yonggyujeong10@gmail.com>
2023-01-09 12:29:29 +0900

    RoundGame의 Controller 추가

---
+++ src/main/java/com/hyunec/cosmicbaseballinit/roundGame/controller/RoundGameController.java
@@ -0,0 +1,57 @@
+package com.hyunec.cosmicbaseballinit.roundGame.controller;
+
+import com.hyunec.cosmicbaseballinit.config.ReadMessageYml;
+import com.hyunec.cosmicbaseballinit.controller.HitterGameInterface;
+import com.hyunec.cosmicbaseballinit.roundGame.domain.HitterGameResultList;
+import com.hyunec.cosmicbaseballinit.roundGame.domain.vo.RoundGameResult;
+import com.hyunec.cosmicbaseballinit.vo.hitterGame.HittingParamVo;
+import com.hyunec.cosmicbaseballinit.vo.hitterGame.PitchProbabilitySettingVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/roundGame")
+public class RoundGameController {
+    private final RoundGameInterface roundGameInterface;
+    private final HitterGameInterface hitterGameInterface;
+    private final ReadMessageYml readMessageYml;
+
+    @GetMapping("/health")
+    public String health() {
+        return "good";
+    }
+
+    @GetMapping("/setting")
+    public String gameSetting() {
+        return hitterGameInterface.gameSetting();
+    }
+
+    @PostMapping("/setting")
+    public String gameSetting(@RequestBody PitchProbabilitySettingVo pitchProbabilitySettingVo) {
+        return hitterGameInterface.gameSetting(pitchProbabilitySettingVo);
+    }
+
+    @GetMapping("/hitting")
+    public String hitting() throws Exception {
+        return roundGameInterface.hitting();
+    }
+
+    @PostMapping("/hitting")
+    public String hitting(@RequestBody HittingParamVo hittingParamVo) throws Exception {
+        return roundGameInterface.hitting(hittingParamVo);
+    }
+
+    @GetMapping("/init")
+    public String initRoundGame() {
+        roundGameInterface.initRoundGame();
+        return readMessageYml.getInitFinish();
+    }
+
+    @GetMapping("/score")
+    public RoundGameResult getScore() {
+        return roundGameInterface.getRoundGameResult();
+    }
+
+
+}
