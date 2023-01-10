6a857662138d8f1b98811bedcbcab06cb1fbcc73
jyg <yonggyujeong10@gmail.com>
2023-01-10 23:05:17 +0900

    Base 클래스 내 주석 javadoc 형태로 변경

--- src/main/java/com/hyunec/cosmicbaseballinit/roundGame/domain/Base.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/roundGame/domain/Base.java
@@ -27,21 +27,31 @@ public class Base {
         return bases.size();
     }
 
-    // 진루: 큐에 1 추가
+    /**
+     * 진루: 큐에 1 추가
+     * @return Base : 1 추가된 Base 객체 반환
+     */
     public Base advancingBase() {
         bases.offer(1);
         return new Base(bases);
     }
 
-    // 홈베이스에 들어온 타자의 수 반환
+    /**
+     * 홈베이스에 들어온 타자의 수 반환
+     * @return Integer : 홈베이스에 들어온 타자의 수
+     */
     public Integer getHitterCountInHomeBase() {
         if (bases.size() >= TOTAL_BASE_COUNT) {
             return (bases.size() - TOTAL_BASE_COUNT);
         }
         return 0;
     }
-
-    // 입력값 만큼 bases에서 제거(홈베이스에 들어온 인원 제거)
+    
+    /**
+     * 입력값 만큼 bases에서 제거(홈베이스에 들어온 인원 제거)
+     * @param count
+     * @return Base: 제거한 후 Base 객체
+     */
     public Base removeHitterInHomeBase(Integer count) {
         for(int i = 0; i < count; i++) {
             bases.poll();
