6157e820439bbc98ae74bd9bd9bb1a1ea3fdbf17
Hyune-c <choi8608@gmail.com>
2023-01-14 06:57:27 +0900

    ✅ 일반 야구 & 우주 야구 lv2 인수 테스트 샘플 추가

---
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/CosmicBaseballLv2Test.java
@@ -0,0 +1,9 @@
+package com.hyunec.cosmicbaseballinit.acceptancetest;
+
+import org.junit.jupiter.api.DisplayName;
+
+class CosmicBaseballLv2Test {
+    @DisplayName("주루 표현에서 3루가 없습니다.")
+    void t1() {
+    }
+}
---
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv2Test.java
@@ -0,0 +1,25 @@
+package com.hyunec.cosmicbaseballinit.acceptancetest;
+
+import org.junit.jupiter.api.DisplayName;
+
+class NormalBaseballLv2Test {
+    @DisplayName("strike, ball, hit 를 가변적으로 입력받을 수 있습니다.")
+    void t1() {
+    }
+
+    @DisplayName("1개의 게임은 3 out 이 되어야 종료됩니다.")
+    void t2() {
+    }
+
+    @DisplayName("타석 상태를 표현할 수 있습니다. strike/ball/out count")
+    void t3() {
+    }
+
+    @DisplayName("주루를 표현할 수 있습니다.")
+    void t4() {
+    }
+
+    @DisplayName("득점을 표현할 수 있습니다.")
+    void t5() {
+    }
+}
