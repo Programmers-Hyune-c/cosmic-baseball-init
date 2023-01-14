4800c8138f336d85cfe2a587fc9ed667a38475e4
Hyune-c <choi8608@gmail.com>
2023-01-14 18:34:04 +0900

    ✅ 일반 야구 & 우주 야구 lv3 인수 테스트 샘플 추가

---
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/CosmicBaseballLv3Test.java
@@ -0,0 +1,21 @@
+package com.hyunec.cosmicbaseballinit.acceptancetest;
+
+import org.junit.jupiter.api.DisplayName;
+
+class CosmicBaseballLv3Test {
+    @DisplayName("fever inning 에는 안타 확률이 2배가 됩니다.")
+    void t1() {
+    }
+
+    @DisplayName("fever inning 에는 득점이 2배가 됩니다.")
+    void t2() {
+    }
+
+    @DisplayName("마지막 회가 아닌 경우 3 out 에서 회가 종료됩니다.")
+    void t3() {
+    }
+
+    @DisplayName("마지막 회는 5 out 에서 회가 종료됩니다.")
+    void t4() {
+    }
+}
---
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv3Test.java
@@ -0,0 +1,13 @@
+package com.hyunec.cosmicbaseballinit.acceptancetest;
+
+import org.junit.jupiter.api.DisplayName;
+
+class NormalBaseballLv3Test {
+    @DisplayName("3번째 회가 3 out 되면 게임을 종료합니다.")
+    void t1() {
+    }
+
+    @DisplayName("지나간 회의 상태를 볼 수 있어야 합니다. - 전광판")
+    void t2() {
+    }
+}
