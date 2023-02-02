8c36c39c8e92722cf90a6496c5364c55a4d16828
kwYoohae <gocksdl7979@gmail.com>
2023-02-02 18:15:13 +0900

    refactor: Batting이 될 수 있는 제일 큰 값 상수로 변경

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/RandomBattingGenerator.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/RandomBattingGenerator.java
@@ -1,17 +1,17 @@
 package com.hyunec.cosmicbaseballinit.domain.baseball.model;
 
-import java.util.Arrays;
 import java.util.Random;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomBattingGenerator implements BattingGenerator {
 
+  private static final int MAX_BATTING_NUMBER = 3;
   private final Random random = new Random();
 
   @Override
   public Batting generator() {
-    return Batting.convertBatting(random.nextInt(2));
+    return Batting.of(random.nextInt() % MAX_BATTING_NUMBER);
   }
 }
 
