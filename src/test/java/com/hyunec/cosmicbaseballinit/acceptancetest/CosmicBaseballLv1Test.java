86542bddb8d7e248092c657733bd4d242fad4771
Hyune-c <choi8608@gmail.com>
2023-02-02 20:37:41 +0900

    ✅ 테스트 구현 없이 실행시 오류가 발생하도록 수정

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/CosmicBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/CosmicBaseballLv1Test.java
@@ -1,25 +1,36 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class CosmicBaseballLv1Test {
     @DisplayName("strike 의 20% 는 bullseye_strike 입니다.")
+    @Test
     void t1() {
+        throw new RuntimeException("Not yet implemented");
     }
 
     @DisplayName("ball 의 20% 는 bullseye_ball 입니다.")
+    @Test
     void t2() {
+        throw new RuntimeException("Not yet implemented");
     }
 
     @DisplayName("hit 의 20% 는 homerun 입니다.")
+    @Test
     void t3() {
+        throw new RuntimeException("Not yet implemented");
     }
 
     @DisplayName("타격 결과가 bullseye_strike 이면 타석 결과는 out 됩니다.")
+    @Test
     void t4() {
+        throw new RuntimeException("Not yet implemented");
     }
 
     @DisplayName("타격 결과가 bullseye_ball 이면 타석 결과는 hit 됩니다.")
+    @Test
     void t5() {
+        throw new RuntimeException("Not yet implemented");
     }
 }
--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/CosmicBaseballLv2Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/CosmicBaseballLv2Test.java
@@ -1,9 +1,12 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class CosmicBaseballLv2Test {
     @DisplayName("주루 표현에서 3루가 없습니다.")
+    @Test
     void t1() {
+        throw new RuntimeException("Not yet implemented");
     }
 }
