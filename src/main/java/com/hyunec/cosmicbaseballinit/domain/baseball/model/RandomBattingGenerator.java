166c838561d3149cbeb5ee070304dddf8d16f2fa
kwYoohae <gocksdl7979@gmail.com>
2023-02-02 19:56:37 +0900

    refactor: 테스트를 위한, Radom객체를 주입받을 수 있도록 초기화 방식 변경

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/RandomBattingGenerator.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/RandomBattingGenerator.java
@@ -1,13 +1,15 @@
 package com.hyunec.cosmicbaseballinit.domain.baseball.model;
 
 import java.util.Random;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class RandomBattingGenerator implements BattingGenerator {
 
   private static final int MAX_BATTING_NUMBER = 3;
-  private final Random random = new Random();
+  private final Random random;
 
   @Override
   public Batting generator() {
