35cd2bad0852c9e335073443e04791c428d8645c
kwYoohae <gocksdl7979@gmail.com>
2023-02-02 22:47:49 +0900

    refactor: Mock을 사용하는 방식으로 테스트 리팩터링

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
@@ -1,17 +1,26 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 import com.hyunec.cosmicbaseballinit.domain.baseball.model.Batting;
 import com.hyunec.cosmicbaseballinit.domain.baseball.model.BattingGenerator;
 import com.hyunec.cosmicbaseballinit.domain.baseball.model.RandomBattingGenerator;
-import java.util.Random;
+import com.hyunec.cosmicbaseballinit.domain.baseball.model.RandomStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class NormalBaseballLv1Test {
+
+
+  @Mock
+  private RandomStrategy randomStrategy;
 
   @DisplayName("strike, ball, hit 는 같은 확률 입니다.")
   @ParameterizedTest
@@ -19,7 +28,8 @@ class NormalBaseballLv1Test {
   void t1(int number) {
     //given
     int MAX_NUMBER = 3;
-    BattingGenerator battingGenerator = new RandomBattingGenerator(createRandom(number));
+    BattingGenerator battingGenerator = new RandomBattingGenerator(randomStrategy);
+    given(randomStrategy.getRandomNumber()).willReturn(number);
 
     //when
     Batting generateValue = battingGenerator.generator();
@@ -50,14 +60,5 @@ class NormalBaseballLv1Test {
   @Test
   void t5() {
     throw new RuntimeException("Not yet implemented");
-  }
-
-  private Random createRandom(int returnValue) {
-    return new Random() {
-      @Override
-      public int nextInt() {
-        return returnValue;
-      }
-    };
   }
 }
