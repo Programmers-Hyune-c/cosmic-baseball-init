26d3e33baba202e2f1deff1d71a853cb752d8651
kwYoohae <gocksdl7979@gmail.com>
2023-02-03 11:34:15 +0900

    refactor(Batting.java): value의 값을 정의하는 Exception 추가

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/Batting.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/Batting.java
@@ -10,12 +10,13 @@ public enum Batting {
   STRIKE(0), BALL(1), HIT(2);
 
   private final Integer value;
+  private static final String VALUE_MUST_BE_SMALLER_THAN_BATTING_SIZE = "Batting으로 변환하려는 숫자 값은 반드시 Batting의 크기보다 작아야합니다.";
 
   public static Batting of(Integer value) {
     return Arrays.stream(Batting.values())
         .filter(e -> e.value.equals(value))
         .findFirst()
-        .orElseThrow();
+        .orElseThrow(() -> new IllegalArgumentException(VALUE_MUST_BE_SMALLER_THAN_BATTING_SIZE));
   }
 
   public static int getBattingSize() {
