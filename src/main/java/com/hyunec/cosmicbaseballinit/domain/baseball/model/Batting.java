fe167c57e7fa13d22227609566e849286280a543
kwYoohae <gocksdl7979@gmail.com>
2023-02-03 11:43:56 +0900

    refactor: Exception에 대한 메세지를 클래스로 분리

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/Batting.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/Batting.java
@@ -1,5 +1,6 @@
 package com.hyunec.cosmicbaseballinit.domain.baseball.model;
 
+import com.hyunec.cosmicbaseballinit.domain.baseball.model.exception.ExceptionMessage;
 import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +11,12 @@ public enum Batting {
   STRIKE(0), BALL(1), HIT(2);
 
   private final Integer value;
-  private static final String VALUE_MUST_BE_SMALLER_THAN_BATTING_SIZE = "Batting으로 변환하려는 숫자 값은 반드시 Batting의 크기보다 작거나 0보다 커야합니다.";
 
   public static Batting of(Integer value) {
     return Arrays.stream(Batting.values())
         .filter(e -> e.value.equals(value))
         .findFirst()
-        .orElseThrow(() -> new IllegalArgumentException(VALUE_MUST_BE_SMALLER_THAN_BATTING_SIZE));
+        .orElseThrow(() -> new IllegalArgumentException(ExceptionMessage.VALUE_MUST_BE_SMALLER_THAN_BATTING_SIZE));
   }
 
   public static int getBattingSize() {
