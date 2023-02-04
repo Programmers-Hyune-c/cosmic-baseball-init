7c37785f5da7a150eaf0cb1114520cdd99f948cb
kwYoohae <gocksdl7979@gmail.com>
2023-02-04 11:02:27 +0900

    refactor: base 타입 변경

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/base/Base.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/base/Base.java
@@ -1,13 +1,15 @@
 package com.hyunec.cosmicbaseballinit.domain.baseball.model.base;
 
+import com.hyunec.cosmicbaseballinit.domain.baseball.model.hit.HitType;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Base {
-  private final List<Integer> base = new ArrayList<>(BaseType.getBaseTypeSize());
 
-  public List<Integer> getBase() {
+  private final List<Boolean> base = new ArrayList<>(BaseType.getBaseTypeSize());
+
+  public List<Boolean> getBase() {
     return Collections.unmodifiableList(base);
   }
 }
