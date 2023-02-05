940dec968cd92722debffdb03c3ddd8ce9633df8
kwYoohae <gocksdl7979@gmail.com>
2023-02-05 20:55:59 +0900

    test: 3B에서 Ball을 한번 더 받을 시 four_ball의 결과를 받는것을 구현

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
@@ -4,23 +4,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 import com.hyunec.cosmicbaseballinit.domain.baseball.model.Batting;
+import com.hyunec.cosmicbaseballinit.domain.baseball.model.BattingResult;
+import com.hyunec.cosmicbaseballinit.domain.baseball.model.PlateAppearances;
+import com.hyunec.cosmicbaseballinit.domain.baseball.model.service.BaseballService;
+import com.hyunec.cosmicbaseballinit.domain.baseball.model.service.BaseballServiceImpl;
 import com.hyunec.cosmicbaseballinit.domain.baseball.model.utils.generator.BattingGenerator;
 import com.hyunec.cosmicbaseballinit.domain.baseball.model.utils.generator.RandomBattingGenerator;
 import com.hyunec.cosmicbaseballinit.domain.baseball.model.utils.generator.radom.RandomStrategy;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class NormalBaseballLv1Test {
-
+  private BaseballService baseballService;
 
   @Mock
   private RandomStrategy randomStrategy;
+
+  @Mock
+  private BattingGenerator battingGenerator;
+
+  @InjectMocks
+  private PlateAppearances plateAppearances;
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+    baseballService = new BaseballServiceImpl(plateAppearances,battingGenerator);
+  }
 
   @DisplayName("strike, ball, hit 는 같은 확률 입니다.")
   @ParameterizedTest
@@ -41,7 +60,17 @@ class NormalBaseballLv1Test {
   @DisplayName("3B 타석에서 타격 결과가 ball 이면 타석 결과는 four_ball 됩니다.")
   @Test
   void t2() {
-    throw new RuntimeException("Not yet implemented");
+    //given
+    given(battingGenerator.generator()).willReturn(Batting.BALL);
+
+    //when
+    baseballService.batting();
+    baseballService.batting();
+    baseballService.batting();
+    baseballService.batting();
+
+    //then
+    assertThat(baseballService.getBattingResult()).isEqualTo(BattingResult.FOUR_BALL);
   }
 
   @DisplayName("2S 타석에서 타격 결과가 strike 이면 타석 결과는 out 됩니다.")
