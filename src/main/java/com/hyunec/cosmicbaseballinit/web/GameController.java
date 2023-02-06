f4696e038a7ae70bbf15375be69453fe067de900
kwYoohae <gocksdl7979@gmail.com>
2023-02-06 14:22:22 +0900

    refactor: ReseponeEntity를 ResponseStatus로 변경

--- src/main/java/com/hyunec/cosmicbaseballinit/web/GameController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/web/GameController.java
@@ -5,8 +5,9 @@ import com.hyunec.cosmicbaseballinit.domain.baseball.model.dto.NewGameResponse;
 import com.hyunec.cosmicbaseballinit.domain.baseball.model.service.BaseballService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -16,15 +17,17 @@ public class GameController {
 
   private final BaseballService baseballService;
 
+  @ResponseStatus(HttpStatus.OK)
   @GetMapping("/game/batting")
-  public ResponseEntity<BattingResult> batting() {
+  public BattingResult batting() {
     baseballService.batting();
     log.info("### plateAppearances.result()={}", baseballService.getBattingResult());
-    return ResponseEntity.ok().body(baseballService.getBattingResult());
+    return baseballService.getBattingResult();
   }
 
+  @ResponseStatus(HttpStatus.OK)
   @GetMapping("/game/new-game")
-  public ResponseEntity<NewGameResponse> newGame() {
-    return ResponseEntity.ok().body(baseballService.newGame());
+  public NewGameResponse newGame() {
+    return baseballService.newGame();
   }
 }
