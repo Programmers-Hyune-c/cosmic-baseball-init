d85fce4d4545d61a3e18fe6c580182cf74f6f727
yuni02 <strangebride@naver.com>
2023-02-11 08:35:49 +0900

    [일반야구 lv1] 테스트 항목 2번 , 3번 통과

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/Batting.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/Batting.java
@@ -6,15 +6,15 @@ import java.util.Arrays;
 import java.util.Random;
 
 @Getter
+
 public enum Batting {
-    STRIKE, BALL, HIT;
+	STRIKE, BALL, HIT;
+	private static final Random random = new Random();
 
-    private static final Random random = new Random();
-
-    public static Batting generate() {
-        return Arrays.stream(Batting.values())
-                .filter(e -> e.ordinal() == random.nextInt(2) + 1)
-                .findFirst()
-                .orElse(STRIKE);
-    }
+	public static Batting generate() {
+		return Arrays.stream(Batting.values())
+				.filter(e -> e.ordinal() == random.nextInt(2) + 1)
+				.findFirst()
+				.orElse(STRIKE);
+	}
 }
---
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/RandomBattingGenerator.java
@@ -0,0 +1,11 @@
+package com.hyunec.cosmicbaseballinit.domain.baseball.model;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RandomBattingGenerator{
+
+
+}
