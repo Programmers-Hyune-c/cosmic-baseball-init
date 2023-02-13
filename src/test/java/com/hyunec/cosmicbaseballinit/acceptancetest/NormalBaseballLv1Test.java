9e14570fa4532bd767ea4192c738ae2cbad78c0b
changmoo <public.ellerymoon@gmail.com>
2023-02-13 20:34:07 +0900

    test: strike, ball, hit 는 같은 확률로 나와야 한다
    deviation 이 5% 미만일 것이라고 가정한 케이스로 랜덤 테스트

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
@@ -1,13 +1,34 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.hyunec.cosmicbaseballinit.domain.baseball.model.Batting;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class NormalBaseballLv1Test {
+
     @DisplayName("strike, ball, hit 는 같은 확률 입니다.")
-    @Test
+    @RepeatedTest(value = 100)
     void t1() {
-        throw new RuntimeException("Not yet implemented");
+        // Given
+        // 3만번 시행시 deviation 이 5% 미만인지 체크한다. 테스트시 strike, ball, hit 가 확률수렴한다
+        int tc = 3 * 10000;
+        double deviation = 0.05;
+
+        // When
+        int total = 0;
+        for (int i = 0; i < tc; i++) {
+            total += Batting.generate().ordinal(); // 1회 배팅 결과를 만드는 메서드를 테스트
+        }
+
+        int lowerBound = (int) Math.floor((double) tc * (1.0 - deviation));
+        int upperBound = (int) Math.floor((double) tc * (1.0 + deviation));
+
+        // Then
+        System.out.println(total);
+        assertThat(total).isBetween(lowerBound, upperBound);
     }
 
     @DisplayName("3B 타석에서 타격 결과가 ball 이면 타석 결과는 four_ball 됩니다.")
