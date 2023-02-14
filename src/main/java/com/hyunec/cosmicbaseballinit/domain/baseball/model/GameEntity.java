8df268c9f270317496ec6b87e5ad40b4c62f63d3
changmoo <public.ellerymoon@gmail.com>
2023-02-15 00:57:17 +0900

    add: Game 정보 엔티티 추가

---
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/GameEntity.java
@@ -0,0 +1,15 @@
+package com.hyunec.cosmicbaseballinit.domain.baseball.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class GameEntity {
+
+    private String message;
+}
