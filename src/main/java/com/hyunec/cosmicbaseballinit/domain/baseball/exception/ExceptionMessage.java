cdf63e4dea564c1dac4fbec8d4d5cb1327fcb2c8
yuni02 <strangebride@naver.com>
2023-02-19 21:29:47 +0900

    REFACTOR: 변수명 변경

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/exception/ExceptionMessage.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/exception/ExceptionMessage.java
@@ -1,7 +1,7 @@
 package com.hyunec.cosmicbaseballinit.domain.baseball.exception;
 
 public class ExceptionMessage {
-        public static final String CANNOT_PROCEED_ATBAT = "현재 타석이 진행중이라, 새로운 게임이 불가능 합니다.";
+        public static final String CANNOT_PROCEED_NEWGAME= "현재 타석이 진행중이라, 새로운 게임이 불가능 합니다.";
         public static final String NEW_GAME_START = "새 게임이 진행됩니다.";
 
 }
