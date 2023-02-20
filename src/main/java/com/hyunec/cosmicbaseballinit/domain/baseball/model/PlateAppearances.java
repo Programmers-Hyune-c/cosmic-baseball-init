fd428a3e95a3ddeb0490f9d3089e8a25ea38423d
yuni02 <strangebride@naver.com>
2023-02-20 19:50:32 +0900

    REFACTOR: 기초적인 컨벤션과 sonarlint 에 따른 포맷팅 적용

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/PlateAppearances.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/baseball/model/PlateAppearances.java
@@ -16,7 +16,7 @@ import com.hyunec.cosmicbaseballinit.domain.baseball.exception.ExceptionMessage;
 @Component
 public class PlateAppearances {
     private final List<Batting> battings = new ArrayList<>();
-    private final Map<String, String> players = new HashMap<String, String>();
+    private final Map<String, String> players = new HashMap<>();
     Integer totalOut = 0;
     boolean isPlaying = false;
 
@@ -49,42 +49,50 @@ public class PlateAppearances {
 
     public boolean fourBallCheck() {
         boolean isFourBallorOut = false;
-        if(strikeCount() ==0 && ballCount()==0) return false;
-        if (result().equals(BattingResult.FOUR_BALL) || result().equals(BattingResult.OUT)) return true;
+        if(strikeCount() == 0 && ballCount() == 0) {
+            return false;
+        }
+        if (result().equals(BattingResult.FOUR_BALL) || result().equals(BattingResult.OUT)) {
+            return true;
+        }
         return isFourBallorOut;
 
     }
 
     public void batting() {
         this.isPlaying = true;
-        if (fourBallCheck()) {
+        if(!fourBallCheck()){
+            batting(Batting.generate());    //4볼이나 OUT이 아니면 리스트에 계속 담는다 > 플레이어 끝
+            batting();
+            return;
+        }
             totalOut++;
             battings.clear();
             this.isPlaying = false;
-            //3OUT - NEW CHANGE GAME
+            if(getTotalOut()<3) {
+                return;
+            }
+            //3OUT - NEW GAME
             if (getTotalOut() == 3) {
                 this.totalOut = 0;
             }
-            return;
-        }
-        batting(Batting.generate());    //4볼이나 OUT이 아니면 리스트에 계속 담는다 > 플레이어 끝
-        batting();
     }
 
 
-    public String newGame() {
-        if (fourBallCheck()) {
-            totalOut = 0;
-            battings.clear();
-            return discriminateNewGame(true);
-        }
-        return discriminateNewGame(false);
+    public boolean newGame() throws Exception {
+       try{
+           if (fourBallCheck()) {
+               totalOut = 0;
+               battings.clear();
+                return true;
+           }else{
+               Exception e = new Exception("고의로 발생시킴");
+               throw e;
+           }
+       }catch(Exception e) {
+           throw new Exception("현재 타석이 진행중이라, 새로운 게임이 불가능 합니다.");
+       }
     }
 
-    //새 게임인지 여부 판별하는 메서드
-    public String discriminateNewGame(boolean isNewGame){
-        if(isNewGame) return ExceptionMessage.NEW_GAME_START;
-        else return ExceptionMessage.CANNOT_PROCEED_NEWGAME;
-    }
 
 }
