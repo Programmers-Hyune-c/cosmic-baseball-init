cbe8b5c06e99c95e8a154d4ccf2639626071b3e3
cohi <tkfka156@gmail.com>
2023-03-03 00:00:25 +0900

    test: 일반 야구 타격 결과 테스트 클래스 수정

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
@@ -1,13 +1,21 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
+ㅑimport static com.hyunec.cosmicbaseballinit.BattingResult.*;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import com.hyunec.cosmicbaseballinit.Batter;
+import com.hyunec.cosmicbaseballinit.BattingResult;
 
 class NormalBaseballLv1Test {
     @DisplayName("타격 결과는 모두 같은 확률을 가집니다.")
     @Test
     void t1() {
-        throw new RuntimeException("Not yet implemented");
+        Batter batter = new Batter();
+        BattingResult result = batter.hit();
+        assertThat(result).isIn(STRIKE, BALL, HIT);
     }
 
     @DisplayName("타격 결과는 strike, ball, hit 입니다.")
