715f10d92b966f12b0bee1fcf8fbb0141f78cee4
cohi <tkfka156@gmail.com>
2023-03-03 18:59:51 +0900

    :sparkles: feat: Random 클래스 빈으로 등록

---
+++ src/main/java/com/hyunec/cosmicbaseballinit/util/UtilConfig.java
@@ -0,0 +1,13 @@
+package com.hyunec.cosmicbaseballinit.util;
+
+import java.util.Random;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UtilConfig {
+    @Bean
+    public Random random(){
+        return new Random();
+    }
+}
