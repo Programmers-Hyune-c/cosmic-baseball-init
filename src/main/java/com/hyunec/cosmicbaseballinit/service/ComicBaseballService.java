2ce5999b2a142cd6a64fbf5c53a9797aefe5bbf6
cohi <tkfka156@gmail.com>
2023-03-03 16:35:32 +0900

    :recycle: refactor: batter 객체 DI

--- src/main/java/com/hyunec/cosmicbaseballinit/service/ComicBaseballService.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/service/ComicBaseballService.java
@@ -5,11 +5,15 @@ import org.springframework.stereotype.Service;
 import com.hyunec.cosmicbaseballinit.domain.Batter;
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class ComicBaseballService {
 
+	private final Batter batter;
+
 	public BattingResult getBattingResult() {
-		Batter batter = new Batter();
 		return batter.hit();
 	}
 }
