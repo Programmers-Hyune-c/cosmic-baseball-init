479f46d2abcf9be0a55cc1102b33ad1bc1711c08
cohi <tkfka156@gmail.com>
2023-03-04 15:58:55 +0900

    :recycle: refactor(CosmicBaseballController): ResponseEntity 제거, @ResponseStatus 추가, dto를 리턴

--- src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
@@ -1,23 +1,23 @@
 package com.hyunec.cosmicbaseballinit.contoller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
-import com.hyunec.cosmicbaseballinit.service.ComicBaseballService;
-
+import com.hyunec.cosmicbaseballinit.service.BattingService;
+import com.hyunec.cosmicbaseballinit.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class CosmicBaseballController {
-	private final ComicBaseballService baseballService;
+	private final BattingService battingService;
 
 	@GetMapping("/batting")
-	public ResponseEntity hit() {
-		BattingResult result = baseballService.getBattingResult();
-		return new ResponseEntity<>(result, HttpStatus.OK);
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseDto hit() {
+		BattingResult result = battingService.hit();
+		return new ResponseDto(result.name());
 	}
 }
