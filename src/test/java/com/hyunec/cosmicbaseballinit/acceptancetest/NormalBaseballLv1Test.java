86b3e37abd226c6ddd7978b3981ebb8c69bc0a38
cohi <tkfka156@gmail.com>
2023-03-04 15:59:43 +0900

    :white_check_mark: test(NormalBaseballLv1Test): 타격 결과 테스트 추가 및 리팩토링

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
@@ -1,57 +1,49 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
-import static org.assertj.core.api.Assertions.*;
+import static com.hyunec.cosmicbaseballinit.domain.BattingResult.BALL;
+import static com.hyunec.cosmicbaseballinit.domain.BattingResult.STRIKE;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.hyunec.cosmicbaseballinit.domain.BattingResult;
+import com.hyunec.cosmicbaseballinit.service.BattingService;
 import java.util.Random;
-
 import org.assertj.core.data.Percentage;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
-
-import com.hyunec.cosmicbaseballinit.domain.Batter;
-import com.hyunec.cosmicbaseballinit.domain.BattingResult;
 
 class NormalBaseballLv1Test {
 
-	private Random random;
+    private Random random = new Random();
+    private BattingService battingService = new BattingService(random);
 
+    @DisplayName("타격 결과는 모두 같은 확률을 가집니다.")
+    @RepeatedTest(10)
+    void t1() {
+        BattingResult battingResult = battingService.hit();
+        int result = getBattingResultCount(battingResult.getIndex());
 
-	@BeforeEach
-	void init() {
-		random = new Random();
-	}
+        assertThat(result).isCloseTo(33_300, Percentage.withPercentage(1));
+    }
 
-	@DisplayName("타격 결과는 모두 같은 확률을 가집니다.")
-	@RepeatedTest(10)
-	void t1() {
-		Batter batter = new Batter();
-		BattingResult battingResult = batter.hit();
-		int count = 0;
-		count = getBattingResultCount(battingResult.ordinal() , count);
+    @DisplayName("타격 결과는 strike, ball, hit 입니다.")
+    @RepeatedTest(10)
+    void t2() {
+        assertThat( battingService.hit()).isBetween(STRIKE, BALL);
+    }
 
-		assertThat(count).isCloseTo(33_300, Percentage.withPercentage(1));
-	}
+    private int getBattingResultCount(int resultNumber) {
+       int count = 0;
+        for (int i = 0; i < 100_000; i++) {
+            int randomNumber = random.nextInt(BattingResult.values().length);
+            count = addCount(resultNumber, count, randomNumber);
+        }
+        return count;
+    }
 
-	@DisplayName("타격 결과는 strike, ball, hit 입니다.")
-	@Test
-	void t2() {
-		throw new RuntimeException("Not yet implemented");
-	}
-
-	private int getBattingResultCount(int resultNumber, int count) {
-		for (int i = 0; i < 100_000; i++) {
-			int randomNumber = random.nextInt(3);
-			count = addCount(resultNumber, count, randomNumber);
-		}
-		return count;
-	}
-
-	private static int addCount(final int resultNumber, int count, final int randomNumber) {
-		if (resultNumber == randomNumber) {
-			count++;
-		}
-		return count;
-	}
+    private int addCount(final int resultNumber, int count, final int randomNumber) {
+        if (resultNumber == randomNumber) {
+           count++;
+        }
+        return count;
+    }
 }
