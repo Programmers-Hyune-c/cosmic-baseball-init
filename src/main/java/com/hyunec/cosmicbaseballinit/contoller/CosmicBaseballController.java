b94c5a81e0a24c2f8de04af93114f67c10222493
cohi <tkfka156@gmail.com>
2023-03-04 17:24:43 +0900

    :recycle: refactor(CosmicBaseballController): ResponseStatus 제거

--- src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
@@ -1,23 +1,21 @@
 package com.hyunec.cosmicbaseballinit.contoller;
 
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
-import com.hyunec.cosmicbaseballinit.service.BattingService;
 import com.hyunec.cosmicbaseballinit.dto.ResponseDto;
+import com.hyunec.cosmicbaseballinit.service.BattingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class CosmicBaseballController {
-	private final BattingService battingService;
 
-	@GetMapping("/batting")
-	@ResponseStatus(HttpStatus.OK)
-	public ResponseDto hit() {
-		BattingResult result = battingService.hit();
-		return new ResponseDto(result.name());
-	}
+    private final BattingService battingService;
+
+    @GetMapping("/batting")
+    public ResponseDto batting() {
+        BattingResult result = battingService.hit();
+        return new ResponseDto(result.name());
+    }
 }
