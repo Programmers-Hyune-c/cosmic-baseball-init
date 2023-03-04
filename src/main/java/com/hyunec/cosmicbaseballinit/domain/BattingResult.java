181b78ade4d94d92a6369aca40e164eac1c36800
cohi <tkfka156@gmail.com>
2023-03-04 15:51:55 +0900

    :sparkles: feat: BattingResult index 추가

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
@@ -1,5 +1,14 @@
 package com.hyunec.cosmicbaseballinit.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum BattingResult {
-	STRIKE, HIT, BALL
+	STRIKE(0), HIT(1), BALL(2);
+
+	private final int index;
+
+	BattingResult(int index) {
+		this.index = index;
+	}
 }
