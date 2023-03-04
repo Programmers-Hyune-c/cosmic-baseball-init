7bfe536341a059b4a271ccb9a44a9258453b39a2
cohi <tkfka156@gmail.com>
2023-03-04 17:24:04 +0900

    :recycle: refactor(BattingResult): index 필드 제거

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
@@ -1,14 +1,5 @@
 package com.hyunec.cosmicbaseballinit.domain;
 
-import lombok.Getter;
-
-@Getter
 public enum BattingResult {
-	STRIKE(0), HIT(1), BALL(2);
-
-	private final int index;
-
-	BattingResult(int index) {
-		this.index = index;
-	}
+    STRIKE, HIT, BALL;
 }
