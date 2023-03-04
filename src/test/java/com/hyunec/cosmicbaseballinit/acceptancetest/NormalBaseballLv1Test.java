84db8500e8e99ca4f2aa5203510afdf111a1d743
cohi <tkfka156@gmail.com>
2023-03-04 17:25:14 +0900

    :white_check_mark: test(NormalBaseballLv1Test): RandomFactory 사용

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
@@ -1,11 +1,11 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
 import static com.hyunec.cosmicbaseballinit.domain.BattingResult.BALL;
+import static com.hyunec.cosmicbaseballinit.domain.BattingResult.HIT;
 import static com.hyunec.cosmicbaseballinit.domain.BattingResult.STRIKE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
-import com.hyunec.cosmicbaseballinit.service.BattingService;
 import java.util.Random;
 import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.DisplayName;
@@ -13,13 +13,12 @@ import org.junit.jupiter.api.RepeatedTest;
 
 class NormalBaseballLv1Test {
 
-    private Random random = new Random();
-    private BattingService battingService = new BattingService(random);
+    private Random random = RandomFactory.getInstance();
 
     @DisplayName("타격 결과는 모두 같은 확률을 가집니다.")
     @RepeatedTest(10)
     void t1() {
-        BattingResult battingResult = battingService.hit();
+        BattingResult battingResult = getResult();
         int result = getBattingResultCount(battingResult.getIndex());
 
         assertThat(result).isCloseTo(33_300, Percentage.withPercentage(1));
@@ -28,11 +27,11 @@ class NormalBaseballLv1Test {
     @DisplayName("타격 결과는 strike, ball, hit 입니다.")
     @RepeatedTest(10)
     void t2() {
-        assertThat( battingService.hit()).isBetween(STRIKE, BALL);
+        assertThat(getResult()).isBetween(STRIKE, BALL);
     }
 
     private int getBattingResultCount(int resultNumber) {
-       int count = 0;
+        int count = 0;
         for (int i = 0; i < 100_000; i++) {
             int randomNumber = random.nextInt(BattingResult.values().length);
             count = addCount(resultNumber, count, randomNumber);
@@ -42,8 +41,19 @@ class NormalBaseballLv1Test {
 
     private int addCount(final int resultNumber, int count, final int randomNumber) {
         if (resultNumber == randomNumber) {
-           count++;
+            count++;
         }
         return count;
+    }
+
+    private BattingResult getResult() {
+        switch (random.nextInt(BattingResult.values().length)) {
+            case 0:
+                return STRIKE;
+            case 1:
+                return HIT;
+            default:
+                return BALL;
+        }
     }
 }
