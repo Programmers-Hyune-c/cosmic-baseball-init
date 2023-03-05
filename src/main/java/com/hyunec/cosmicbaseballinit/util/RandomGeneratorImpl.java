75d6f10aa6ca624143814ec91105f903cbc84b72
cohi <tkfka156@gmail.com>
2023-03-06 00:11:24 +0900

    :recycle: feat(RandomGeneratorImpl): random 객체 di

--- src/main/java/com/hyunec/cosmicbaseballinit/util/RandomGeneratorImpl.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/util/RandomGeneratorImpl.java
@@ -2,10 +2,12 @@ package com.hyunec.cosmicbaseballinit.util;
 
 
 import java.util.Random;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class RandomGeneratorImpl implements RandomGenerator {
 
-    private final Random random = new Random();
+    private final Random random;
 
     public int getNumber(int bound) {
         return random.nextInt(bound);
