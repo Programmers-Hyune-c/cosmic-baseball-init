96d47b812e5811cba07ee26fb5ef2cd95416d34d
cohi <tkfka156@gmail.com>
2023-03-06 19:15:00 +0900

    :white_check_mark: test(NormalBaseballLv1Test): 테스트 메서드 변경

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/CosmicBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/CosmicBaseballLv1Test.java
@@ -1,13 +1,8 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
-import static com.hyunec.cosmicbaseballinit.BattingResult.*;
-import static org.assertj.core.api.AssertionsForClassTypes.*;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import com.hyunec.cosmicbaseballinit.Batter;
-import com.hyunec.cosmicbaseballinit.BattingResult;
 
 class CosmicBaseballLv1Test {
 	@DisplayName("타격 결과는 모두 같은 확률을 가집니다.")
