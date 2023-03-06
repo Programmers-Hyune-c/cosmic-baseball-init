f0760b2e2378e6b38c67e8279f516dcc025dc315
cohi <tkfka156@gmail.com>
2023-03-06 20:59:32 +0900

    :sparkles: feat(CosmicBaseballController): ballCountService 로직 추가

--- src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
@@ -1,7 +1,10 @@
 package com.hyunec.cosmicbaseballinit.contoller;
 
+import static com.hyunec.cosmicbaseballinit.service.BallCountService.COUNT_STORE;
+
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
 import com.hyunec.cosmicbaseballinit.dto.ResponseDto;
+import com.hyunec.cosmicbaseballinit.service.BallCountService;
 import com.hyunec.cosmicbaseballinit.service.BattingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class CosmicBaseballController {
 
     private final BattingService battingService;
+    private final BallCountService ballCountService;
 
     @GetMapping("/batting")
     public ResponseDto startBatting() {
         BattingResult result = battingService.batting();
-        return new ResponseDto(result.name());
+        ballCountService.setBallCount(result);
+
+        return new ResponseDto(result.name(), COUNT_STORE);
     }
 }
