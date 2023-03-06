9554452303c4dda0a0eb33fb498661a9309cde96
cohi <tkfka156@gmail.com>
2023-03-06 16:59:31 +0900

    :white_check_mark: test(BattingService): 테스트 코드 리팩토링 (values 이용)

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
@@ -1,59 +1,39 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
 import static com.hyunec.cosmicbaseballinit.domain.BattingResult.BALL;
-import static com.hyunec.cosmicbaseballinit.domain.BattingResult.HIT;
 import static com.hyunec.cosmicbaseballinit.domain.BattingResult.STRIKE;
+import static com.hyunec.cosmicbaseballinit.service.BattingService.RANDOM;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
-import java.util.Random;
 import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 
 class NormalBaseballLv1Test {
 
-    private Random random = RandomFactory.getInstance();
-
     @DisplayName("타격 결과는 모두 같은 확률을 가집니다.")
     @RepeatedTest(10)
     void t1() {
-        BattingResult battingResult = getResult();
-        int result = getBattingResultCount(battingResult.ordinal());
-
-        assertThat(result).isCloseTo(33_300, Percentage.withPercentage(1));
+        assertThat(getCount()).isCloseTo(33_300, Percentage.withPercentage(1));
     }
 
     @DisplayName("타격 결과는 strike, ball, hit 입니다.")
     @RepeatedTest(10)
     void t2() {
-        assertThat(getResult()).isBetween(STRIKE, BALL);
+        BattingResult result = BattingResult.values()[RANDOM.nextInt(
+            BattingResult.values().length)];
+        assertThat(result).isBetween(STRIKE, BALL);
     }
 
-    private int getBattingResultCount(int resultNumber) {
+    private int getCount() {
         int count = 0;
-        for (int i = 0; i < 100_000; i++) {
-            int randomNumber = random.nextInt(BattingResult.values().length);
-            count = addCount(resultNumber, count, randomNumber);
+        for (int i = 0; i <100000 ; i++) {
+            BattingResult result = BattingResult.values()[RANDOM.nextInt(BattingResult.values().length)];
+            if (result == STRIKE){
+                count++;
+            }
         }
         return count;
-    }
-
-    private int addCount(final int resultNumber, int count, final int randomNumber) {
-        if (resultNumber == randomNumber) {
-            count++;
-        }
-        return count;
-    }
-
-    private BattingResult getResult() {
-        switch (random.nextInt(BattingResult.values().length)) {
-            case 0:
-                return STRIKE;
-            case 1:
-                return HIT;
-            default:
-                return BALL;
-        }
     }
 }
