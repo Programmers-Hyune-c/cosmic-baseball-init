eb283cfd05ebe9b306b12b9ef95ec848ad422a84
cohi <tkfka156@gmail.com>
2023-03-06 20:57:09 +0900

    :sparkles: feat(BattingResult): enum 값에 메서드 오버라이드 적용

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
@@ -1,5 +1,39 @@
 package com.hyunec.cosmicbaseballinit.domain;
 
-public enum BattingResult {
-    STRIKE, HIT, BALL;
+import static com.hyunec.cosmicbaseballinit.service.BallCountService.COUNT_STORE;
+
+import com.hyunec.cosmicbaseballinit.service.BallCountService;
+
+public enum BattingResult implements BallCountSetter {
+    STRIKE {
+        @Override
+        public void call() {
+            COUNT_STORE.compute("STRIKE", (k, v) -> v + 1);
+        }
+    },
+    BALL{
+        @Override
+        public void call() {
+            COUNT_STORE.compute("BALL", (k, v) -> v + 1);
+        }
+    },
+    DOUBLE_STRIKE{
+        @Override
+        public void call() {
+            COUNT_STORE.compute("STRIKE", (k, v) -> v + 2);
+        }
+    },
+    DOUBLE_BALL{
+        @Override
+        public void call() {
+            COUNT_STORE.compute("BALL", (k, v) -> v + 2);
+        }
+    },
+    HIT{
+        @Override
+        public void call() {
+            BallCountService.resetBallCount();
+        }
+    };
+
 }
