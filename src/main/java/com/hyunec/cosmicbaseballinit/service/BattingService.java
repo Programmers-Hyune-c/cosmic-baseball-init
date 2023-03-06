4f2d639fc7381064dc6624083c73dd7010656c31
cohi <tkfka156@gmail.com>
2023-03-06 16:56:32 +0900

    :recycle: refactor(BattingService): switch문 제거

--- src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
@@ -1,33 +1,16 @@
 package com.hyunec.cosmicbaseballinit.service;
 
-import static com.hyunec.cosmicbaseballinit.domain.BattingResult.BALL;
-import static com.hyunec.cosmicbaseballinit.domain.BattingResult.HIT;
-import static com.hyunec.cosmicbaseballinit.domain.BattingResult.STRIKE;
-
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
 import java.util.Random;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class BattingService {
 
-	private static final Random RANDOM_GENERATOR = new Random();
+	public static final Random RANDOM = new Random();
 
 	public BattingResult batting() {
-		return getResult();
+		return BattingResult.values()[RANDOM.nextInt(BattingResult.values().length)];
 	}
 
-	private BattingResult getResult() {
-
-		switch (RANDOM_GENERATOR.nextInt(BattingResult.values().length)) {
-			case 0:
-				return STRIKE;
-			case 1:
-				return HIT;
-			default:
-				return BALL;
-		}
-	}
 }
