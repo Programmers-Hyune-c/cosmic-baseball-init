064ed2dd894c5fbbf7cd5cd90c4df5877ee5260a
cohi <tkfka156@gmail.com>
2023-03-07 20:27:35 +0900

    :sparkles: feat: 타격 결과 카운트 로직 추가

--- src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
@@ -3,6 +3,8 @@ package com.hyunec.cosmicbaseballinit.contoller;
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
 import com.hyunec.cosmicbaseballinit.dto.ResponseDto;
 import com.hyunec.cosmicbaseballinit.service.BattingService;
+import com.hyunec.cosmicbaseballinit.service.ResultCountService;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CosmicBaseballController {
 
     private final BattingService battingService;
+    private final ResultCountService resultCountService;
 
     @GetMapping("/batting")
     public ResponseDto startBatting() {
+
         BattingResult result = battingService.batting();
-        return new ResponseDto(result.getName());
+
+        resultCountService.addResultCount(result);
+        Map<String, Integer> resultCount = resultCountService.getResultCount();
+
+        return new ResponseDto(result.getName(), resultCount);
     }
 }
