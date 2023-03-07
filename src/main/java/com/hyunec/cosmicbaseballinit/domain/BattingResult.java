f6f8da7836c9ce60b4b29857ed6460b206e16a53
cohi <tkfka156@gmail.com>
2023-03-07 20:22:43 +0900

    :sparkles: feat: 타격 결과 카운트를 반영하는 메서드 정의

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
@@ -1,5 +1,10 @@
 package com.hyunec.cosmicbaseballinit.domain;
 
+import java.util.Map;
+
 public interface BattingResult {
+
     String getName();
+
+    void call(Map<String, Integer> resultCount);
 }
