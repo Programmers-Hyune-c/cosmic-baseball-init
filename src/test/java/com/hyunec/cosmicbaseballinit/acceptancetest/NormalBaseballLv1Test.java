f8209dad8cca9c5432b32df34f1105c4e3763853
cohi <tkfka156@gmail.com>
2023-03-07 17:45:16 +0900

    :white_check_mark: test: 테스트 수정

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
@@ -1,8 +1,6 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
-import static com.hyunec.cosmicbaseballinit.domain.BattingResult.BALL;
-import static com.hyunec.cosmicbaseballinit.domain.BattingResult.HIT;
-import static com.hyunec.cosmicbaseballinit.domain.BattingResult.STRIKE;
+import static com.hyunec.cosmicbaseballinit.service.BattingService.BATTING_RESULTS;
 import static com.hyunec.cosmicbaseballinit.service.BattingService.RANDOM;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,21 +14,22 @@ class NormalBaseballLv1Test {
     @DisplayName("타격 결과는 모두 같은 확률을 가집니다.")
     @RepeatedTest(10)
     void t1() {
-        assertThat(getPickedCount()).isCloseTo(33_300, Percentage.withPercentage(1));
+        assertThat(getTotalRandomPick()).isCloseTo(33_300, Percentage.withPercentage(1));
     }
 
     @DisplayName("타격 결과는 strike, ball, hit 입니다.")
     @RepeatedTest(10)
     void t2() {
-        BattingResult result = BattingResult.values()[RANDOM.nextInt(BattingResult.values().length)];
-        assertThat(result).isIn(STRIKE, BALL, HIT);
+        BattingResult result = BATTING_RESULTS.get(RANDOM.nextInt(BATTING_RESULTS.size()));
+        assertThat(result).
+                        isIn(BATTING_RESULTS.get(0), BATTING_RESULTS.get(1), BATTING_RESULTS.get(2));
     }
 
-    private int getPickedCount() {
+    private int getTotalRandomPick() {
         int count = 0;
-        for (int i = 0; i <100000 ; i++) {
-            BattingResult result = BattingResult.values()[RANDOM.nextInt(BattingResult.values().length)];
-            if (result == STRIKE){
+        for (int i = 0; i < 100000; i++) {
+            BattingResult result = BATTING_RESULTS.get(RANDOM.nextInt(BATTING_RESULTS.size()));
+            if (result == BATTING_RESULTS.get(0)) {
                 count++;
             }
         }
