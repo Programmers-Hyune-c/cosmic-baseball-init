88bebf7cb013450819ba4d9b4fa88c08ada79692
cohi <tkfka156@gmail.com>
2023-03-09 05:25:47 +0900

    :recycle: refactor: BattingResult 인터페이스 call 메서드 정의 변경

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
@@ -1,10 +1,8 @@
 package com.hyunec.cosmicbaseballinit.domain;
 
-import java.util.Map;
-
 public interface BattingResult {
 
     String getName();
 
-    void call(Map<String, Integer> resultCount);
+    int call();
 }
