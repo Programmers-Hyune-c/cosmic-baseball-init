c1f23a2be377dd9a3a2b474463adb67f6d47f89d
cohi <tkfka156@gmail.com>
2023-03-10 07:03:54 +0900

    :sparkles: feat: id 값 추가

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResultCount.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResultCount.java
@@ -1,10 +1,13 @@
 package com.hyunec.cosmicbaseballinit.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class BattingResultCount {
 
+    @Setter
+    private long id;
     private int ballCount;
     private int strikeCount;
 
@@ -26,11 +29,12 @@ public class BattingResultCount {
                 this.strikeCount += count;
                 break;
             case "Ball":
+            case "DoubleBall":
                 this.ballCount += count;
                 break;
             default:
-                this.strikeCount = 0;
-                this.ballCount = 0;
+                this.strikeCount = count;
+                this.ballCount = count;
         }
     }
 }
