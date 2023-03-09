582e36065e3c56b8924e708a09e876a8117b0540
cohi <tkfka156@gmail.com>
2023-03-10 07:11:11 +0900

    :recycle: refactor: 정적 팩토리 메서드로 구성

--- src/main/java/com/hyunec/cosmicbaseballinit/dto/ResponseDto.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/dto/ResponseDto.java
@@ -1,19 +1,30 @@
 package com.hyunec.cosmicbaseballinit.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.hyunec.cosmicbaseballinit.domain.BattingResultCount;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 
-@Getter
+@Data
+@JsonInclude(Include.NON_NULL)
 public class ResponseDto {
 
     private final String result;
-    private final int strikeCount;
-    private final int ballCount;
+    private final BattingResultCount resultCount;
 
     @Builder
-    public ResponseDto(String result, int strikeCount, int ballCount) {
+    private ResponseDto(String result, BattingResultCount resultCount) {
         this.result = result;
-        this.strikeCount = strikeCount;
-        this.ballCount = ballCount;
+        this.resultCount = resultCount;
+    }
+
+    public static ResponseDto of(BattingResultCount battingResultCount) {
+        return ResponseDto.builder().resultCount(battingResultCount).build();
+    }
+
+    public static ResponseDto of(String result, BattingResultCount battingResultCount) {
+        return ResponseDto.builder().result(result).resultCount(battingResultCount).build();
     }
 }
+
