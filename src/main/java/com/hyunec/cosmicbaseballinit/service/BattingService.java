627c808bb28de6c02467ebc8b7b618ea7f022fb7
cohi <tkfka156@gmail.com>
2023-03-10 07:08:22 +0900

    :sparkles: feat: double ball 추가, 타격 시작 시 로직 구성

--- src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
@@ -1,24 +1,38 @@
 package com.hyunec.cosmicbaseballinit.service;
 
+import com.hyunec.cosmicbaseballinit.dao.BattingResultCountDao;
 import com.hyunec.cosmicbaseballinit.domain.Ball;
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
+import com.hyunec.cosmicbaseballinit.domain.BattingResultCount;
+import com.hyunec.cosmicbaseballinit.domain.DoubleBall;
 import com.hyunec.cosmicbaseballinit.domain.DoubleStrike;
 import com.hyunec.cosmicbaseballinit.domain.Hit;
 import com.hyunec.cosmicbaseballinit.domain.Strike;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BattingService {
 
-    private final List<BattingResult> battingResult =
-                new ArrayList<>(List.of(new Strike(), new Hit(), new Ball(), new DoubleStrike()));
+    private final BattingResultCountDao battingResultCountDao;
+
+    private static final List<BattingResult> battingResult =
+        BattingResultGenerator.of(
+            new Strike(), new Ball(), new Hit(), new DoubleStrike(), new DoubleBall()
+        );
 
     public static final Random RANDOM = new Random();
 
     public BattingResult batting() {
         return battingResult.get(RANDOM.nextInt(battingResult.size()));
+    }
+
+    public BattingResultCount startBatting() {
+        BattingResultCount battingResultCount = BattingResultCount.getInstance();
+        battingResultCountDao.save(battingResultCount);
+        return battingResultCount;
     }
 }
