6b02c454a10f466bfe98770ec6dc59e5e63bd98f
cohi <tkfka156@gmail.com>
2023-03-13 01:41:28 +0900

    :recycle: refactor: setBattingTotalResult 메서드 세분화

--- src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
@@ -2,7 +2,7 @@ package com.hyunec.cosmicbaseballinit.service;
 
 import static com.hyunec.cosmicbaseballinit.domain.BatterStatus.ON_GOING;
 
-import com.hyunec.cosmicbaseballinit.dao.BattingResultCountDao;
+import com.hyunec.cosmicbaseballinit.dao.TotalBattingResultDao;
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
 import com.hyunec.cosmicbaseballinit.domain.TotalBattingResult;
 import com.hyunec.cosmicbaseballinit.exception.NewBattingException;
@@ -14,23 +14,26 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BattingService {
 
-    private final BattingResultCountDao battingResultCountDao;
+    private final TotalBattingResultDao totalBattingResultDao;
 
     public static final Random RANDOM = new Random();
 
     public TotalBattingResult startBatting() {
         TotalBattingResult totalBattingResult = TotalBattingResult.getInstance();
-        battingResultCountDao.save(totalBattingResult);
+        totalBattingResultDao.save(totalBattingResult);
 
         return totalBattingResult;
     }
 
     public TotalBattingResult batting(Long id) {
-        TotalBattingResult totalBattingResultEntity = battingResultCountDao.findById(id);
+        TotalBattingResult totalBattingResultEntity = totalBattingResultDao.findById(id);
         BattingResult result = BattingResult.values()[RANDOM.nextInt(BattingResult.values().length)];
 
-        totalBattingResultEntity.setBattingTotalResult(result);
-        battingResultCountDao.update(id, totalBattingResultEntity);
+        totalBattingResultEntity.setBattingResult(result);
+        totalBattingResultEntity.addBattingResultCount(result);
+        totalBattingResultEntity.judgeBatterStatus();
+
+        totalBattingResultDao.update(id, totalBattingResultEntity);
 
         return totalBattingResultEntity;
     }
@@ -39,12 +42,12 @@ public class BattingService {
         if (isOnGoing(id)) {
             throw new NewBattingException("새로운 타석 안됨");
         }
-        battingResultCountDao.delete(id);
+        totalBattingResultDao.delete(id);
         return startBatting();
     }
 
     private boolean isOnGoing(Long id) {
-        TotalBattingResult totalBattingResultEntity = battingResultCountDao.findById(id);
+        TotalBattingResult totalBattingResultEntity = totalBattingResultDao.findById(id);
         return totalBattingResultEntity.getBatterStatus() == ON_GOING;
     }
 }
