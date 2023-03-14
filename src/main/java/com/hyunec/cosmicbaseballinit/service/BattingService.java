69301d2ea4eb83b9c8449dca346b842140355281
cohi <tkfka156@gmail.com>
2023-03-14 14:49:59 +0900

    :recycle: refactor: atomiclong 제거, 검증 메서드 제거

--- src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
@@ -1,13 +1,9 @@
 package com.hyunec.cosmicbaseballinit.service;
 
-import static com.hyunec.cosmicbaseballinit.domain.BatterStatus.ON_GOING;
-
 import com.hyunec.cosmicbaseballinit.dao.TotalBattingResultDao;
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
 import com.hyunec.cosmicbaseballinit.domain.TotalBattingResult;
-import com.hyunec.cosmicbaseballinit.exception.NewBattingException;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +12,11 @@ import org.springframework.stereotype.Service;
 public class BattingService {
 
     private final TotalBattingResultDao totalBattingResultDao;
-    private final AtomicLong sequence = new AtomicLong(0);
 
     public static final Random RANDOM = new Random();
 
     public TotalBattingResult startBatting() {
-        long id = sequence.incrementAndGet();
-        TotalBattingResult startBatting = new TotalBattingResult(id);
-
+        TotalBattingResult startBatting = new TotalBattingResult();
         return totalBattingResultDao.save(startBatting);
     }
 
@@ -35,19 +28,8 @@ public class BattingService {
         totalBattingResultEntity.addBattingResultCount(result);
         totalBattingResultEntity.judgeBatterStatus();
 
-        return totalBattingResultDao.update(id, totalBattingResultEntity);
+        return totalBattingResultDao.update(totalBattingResultEntity);
     }
 
-    public TotalBattingResult newBatting(Long id) {
-        if (isOnGoing(id)) {
-            throw new NewBattingException("새로운 타석 안됨");
-        }
-        totalBattingResultDao.delete(id);
-        return startBatting();
-    }
 
-    private boolean isOnGoing(Long id) {
-        TotalBattingResult totalBattingResultEntity = totalBattingResultDao.findById(id);
-        return totalBattingResultEntity.getBatterStatus() == ON_GOING;
-    }
 }
