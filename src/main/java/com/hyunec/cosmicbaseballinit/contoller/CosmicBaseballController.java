3e01c3e255c71a2abd7b27ec6f5b5f7812181ca1
cohi <tkfka156@gmail.com>
2023-03-14 14:46:52 +0900

    :sparkles: feat: 타석 진행 중 검증 로직 추가 및 중복 메서드 제거

--- src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
@@ -1,12 +1,17 @@
 package com.hyunec.cosmicbaseballinit.contoller;
 
+import static com.hyunec.cosmicbaseballinit.domain.BatterStatus.ON_GOING;
+
 import com.hyunec.cosmicbaseballinit.domain.TotalBattingResult;
 import com.hyunec.cosmicbaseballinit.dto.ResponseDto;
+import com.hyunec.cosmicbaseballinit.exception.NewBattingException;
 import com.hyunec.cosmicbaseballinit.service.BattingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,10 +21,13 @@ public class CosmicBaseballController {
     private final BattingService battingService;
 
     @PostMapping("/batting/start")
-    public ResponseDto startBatting() {
+    public ResponseDto startBatting(@Nullable @RequestParam("status") String status) {
+        validateOnGoing(status);
+
         TotalBattingResult startBatting = battingService.startBatting();
         return new ResponseDto(startBatting);
     }
+
 
     @PatchMapping("/batting/{id}")
     public ResponseDto batting(@PathVariable Long id) {
@@ -27,9 +35,13 @@ public class CosmicBaseballController {
         return new ResponseDto(totalBattingResult);
     }
 
-    @PostMapping("/batting/new/{id}")
-    public ResponseDto newBatting(@PathVariable Long id) {
-        TotalBattingResult newBatting = battingService.newBatting(id);
-        return new ResponseDto(newBatting);
+    private void validateOnGoing(String status) {
+        if (isOnGoing(status)) {
+            throw new NewBattingException("새로운 타석 안됨");
+        }
     }
+
+        private  boolean isOnGoing(String status) {
+            return status != null && status.equals(ON_GOING.name());
+        }
 }
