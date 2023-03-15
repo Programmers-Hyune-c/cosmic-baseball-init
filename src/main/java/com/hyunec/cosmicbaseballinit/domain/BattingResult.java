7af9adc78d414f468e2437bf88abe69ad21f25c6
cohi <tkfka156@gmail.com>
2023-03-15 15:53:29 +0900

    :sparkles: feat(BattingResult): bull eye strike, bull eye ball 추가

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
@@ -4,11 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum BattingResult {
+    BULL_EYE_STRIKE(3,0),
+    BULL_EYE_BALL(0,4),
     STRIKE(1,0),
     BALL(0,1),
     HIT(0,0),
     DOUBLE_STRIKE(2,0),
     DOUBLE_BALL(0,2);
+
 
 
     private final int increaseStrikeCount;
