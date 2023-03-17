4acce262516f9d83e879e58b4a1262c56f37bf44
cohi <tkfka156@gmail.com>
2023-03-17 09:09:12 +0900

    :recycle: refactor(BattingResult): enum값 위치 변경

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/BattingResult.java
@@ -1,22 +1,20 @@
 package com.hyunec.cosmicbaseballinit.domain;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum BattingResult {
-    BULL_EYE_STRIKE(3,0),
-    BULL_EYE_BALL(0,4),
-    STRIKE(1,0),
-    BALL(0,1),
-    HIT(0,0),
-    DOUBLE_STRIKE(2,0),
-    DOUBLE_BALL(0,2);
+    STRIKE(1, 0),
+    BALL(0, 1),
+    HIT(0, 0),
+    DOUBLE_STRIKE(2, 0),
+    DOUBLE_BALL(0, 2),
+    BULL_EYE_STRIKE(3, 0),
+    BULL_EYE_BALL(0, 4);
 
     private final int increaseStrikeCount;
     private final int increaseBallCount;
 
-    BattingResult(int increaseStrikeCount, int increaseBallCount) {
-        this.increaseStrikeCount = increaseStrikeCount;
-        this.increaseBallCount = increaseBallCount;
-    }
 }
