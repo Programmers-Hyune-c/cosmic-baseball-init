7b0ab317cc0aa01e8363869bb0f716d6fbe5ccb8
cohi <tkfka156@gmail.com>
2023-03-18 12:52:26 +0900

    :sparkles: refactor(BattingService): ddfRanRandomBattingResultGenerator di 및 메서드 리팩토링

--- src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
@@ -1,14 +1,8 @@
 package com.hyunec.cosmicbaseballinit.service;
 
-import static com.hyunec.cosmicbaseballinit.domain.BattingResult.BALL;
-import static com.hyunec.cosmicbaseballinit.domain.BattingResult.BULL_EYE_BALL;
-import static com.hyunec.cosmicbaseballinit.domain.BattingResult.BULL_EYE_STRIKE;
-import static com.hyunec.cosmicbaseballinit.domain.BattingResult.STRIKE;
-
 import com.hyunec.cosmicbaseballinit.dao.TotalBattingResultDao;
 import com.hyunec.cosmicbaseballinit.domain.BattingResult;
 import com.hyunec.cosmicbaseballinit.domain.TotalBattingResult;
-import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,18 +11,15 @@ import org.springframework.stereotype.Service;
 public class BattingService {
 
     private final TotalBattingResultDao totalBattingResultDao;
-
-    public static final Random RANDOM = new Random();
-    private static final int RANGE = 5;
+    private final RandomBattingResultGenerator randomBattingResultGenerator;
 
     public TotalBattingResult newBatting() {
         return totalBattingResultDao.save(new TotalBattingResult());
     }
 
     public TotalBattingResult batting(Long id) {
-        TotalBattingResult updatedTotalBattingResult
-                         = updateTotalBattingResult(id, getBattingResult());
-        return totalBattingResultDao.update(updatedTotalBattingResult);
+        BattingResult battingResult = randomBattingResultGenerator.getBattingResult();
+        return updateTotalBattingResult(id, battingResult);
     }
 
     private TotalBattingResult updateTotalBattingResult(
@@ -37,32 +28,8 @@ public class BattingService {
     ) {
         TotalBattingResult totalBattingResultEntity = totalBattingResultDao.findById(id);
         totalBattingResultEntity.setBattingResult(result);
-        totalBattingResultEntity.addBattingResultCount(result);
+        totalBattingResultEntity.increaseBattingResultCount(result);
         totalBattingResultEntity.judgeBatterStatus();
         return totalBattingResultEntity;
-    }
-
-    private BattingResult getBattingResult() {
-        BattingResult result = getRandomResult();
-        switch (result) {
-            case STRIKE:
-                return isBullEyeStrike() ? BULL_EYE_STRIKE : STRIKE;
-            case BALL:
-                return isBullEyeBall() ? BULL_EYE_BALL : BALL;
-            default:
-                return result;
-        }
-    }
-
-    private  BattingResult getRandomResult() {
-        return BattingResult.values()[RANDOM.nextInt(RANGE)];
-    }
-
-    private boolean isBullEyeStrike() {
-        return RANDOM.nextInt(RANGE) == STRIKE.ordinal();
-    }
-
-    private boolean isBullEyeBall() {
-        return RANDOM.nextInt(RANGE) == BALL.ordinal();
     }
 }
