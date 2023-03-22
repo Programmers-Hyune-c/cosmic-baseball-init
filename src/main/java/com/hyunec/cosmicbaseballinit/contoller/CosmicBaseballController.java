32d46726fd45471f42d5ec2e150729ec3c347669
cohi <tkfka156@gmail.com>
2023-03-22 21:22:17 +0900

    :sparkles: feat(CosmicBaseballController): 게임 종료 체크 로직 추가

--- src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/contoller/CosmicBaseballController.java
@@ -3,18 +3,25 @@ package com.hyunec.cosmicbaseballinit.contoller;
 import static com.hyunec.cosmicbaseballinit.domain.BatterStatus.ON_GOING;
 
 import com.hyunec.cosmicbaseballinit.domain.TotalBattingResult;
+import com.hyunec.cosmicbaseballinit.dto.BattingPatchDto;
 import com.hyunec.cosmicbaseballinit.dto.ResponseDto;
-import com.hyunec.cosmicbaseballinit.exception.NewBattingException;
+import com.hyunec.cosmicbaseballinit.exception.BusinessException;
+import com.hyunec.cosmicbaseballinit.exception.ExceptionType;
 import com.hyunec.cosmicbaseballinit.service.BattingService;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.Nullable;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@Validated
 @RestController
 public class CosmicBaseballController {
 
@@ -28,18 +35,28 @@ public class CosmicBaseballController {
     }
 
     @PatchMapping("/batting/{id}")
-    public ResponseDto batting(@PathVariable Long id) {
-        TotalBattingResult totalBattingResult = battingService.batting(id);
+    public ResponseDto batting(@Min(1) @PathVariable Long id, @Valid @RequestBody BattingPatchDto patchDto) {
+
+        checkOutCount(patchDto.getOutCount());
+
+        TotalBattingResult totalBattingResult =
+            battingService.batting(id, patchDto.getPercentage(), patchDto.getTargetResult());
         return new ResponseDto(totalBattingResult);
+    }
+
+    private void checkOutCount(int outCount) {
+        if (outCount >= 3){
+            throw new BusinessException(ExceptionType.END_INNING);
+        }
     }
 
     private void validateOnGoing(String status) {
         if (isOnGoing(status)) {
-            throw new NewBattingException("새로운 타석 안됨");
+            throw new BusinessException(ExceptionType.NEW_BATTING);
         }
     }
 
-        private  boolean isOnGoing(String status) {
-            return status != null && status.equals(ON_GOING.name());
-        }
+    private boolean isOnGoing(String status) {
+        return status != null && status.equals(ON_GOING.name());
+    }
 }
