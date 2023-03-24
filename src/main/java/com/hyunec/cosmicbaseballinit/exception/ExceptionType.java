b2b6a4582edcfbbda4ef3170a3e6dfac24f11462
cohi <tkfka156@gmail.com>
2023-03-24 14:54:59 +0900

    :recycle: refactor(ExceptionType): 생성자 롬복 적용

--- src/main/java/com/hyunec/cosmicbaseballinit/exception/ExceptionType.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/exception/ExceptionType.java
@@ -1,14 +1,13 @@
 package com.hyunec.cosmicbaseballinit.exception;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Getter
 public enum ExceptionType {
     NEW_BATTING("타석이 진행 중 일때, 새로운 타석은 불가능합니다."),
     END_INNING("이닝이 종료되어 게임이 종료됩니다.");
 
-    private String message;
-    ExceptionType(String message) {
-       this.message = message;
-    }
+    private final String message;
 }
