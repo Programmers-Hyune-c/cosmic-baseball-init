c534aa38950a4260011f00563a58ae161a71976f
cohi <tkfka156@gmail.com>
2023-03-24 15:42:42 +0900

    :recycle: refactor(ScoreAndBaseBoard): baseNumber 필드 생성, 테스트용 생성자 생성 , private 메서 생성(doScoringProcess)

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/ScoreAndBaseBoard.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/ScoreAndBaseBoard.java
@@ -3,20 +3,35 @@ package com.hyunec.cosmicbaseballinit.domain;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ScoreAndBaseBoard {
 
     private static final int COUNT_OF_BASES = 4;
     private final List<Integer> onBaseList = new ArrayList<>();
     private int score;
+    private int baseNumber;
 
-    public void adjustBaseAndScore(int baseCountToAdvance) {
-        onBaseList.add(baseCountToAdvance);
+    public ScoreAndBaseBoard(int baseNumber) {
+        onBaseList.add(baseNumber);
+        this.baseNumber = baseNumber;
+    }
+
+    public void adjustBaseAndScore() {
+        onBaseList.add(++baseNumber);
         if (isRun()) {
-            score++;
+            doScoringProcess();
         }
     }
+
+    private void doScoringProcess() {
+        score++;
+        onBaseList.clear();
+        baseNumber = 0;
+    }
+
     private boolean isRun() {
         return COUNT_OF_BASES == onBaseList.get(onBaseList.size() - 1);
     }
