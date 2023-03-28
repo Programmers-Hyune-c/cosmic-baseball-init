cce11f0fc99e77ea9e6fd08ec3e296dcb29e3cf7
cohi <tkfka156@gmail.com>
2023-03-28 21:42:53 +0900

    :sparkles: feat: fever inning시 3루 없애고 바로 득점하는 로직 구현

--- src/main/java/com/hyunec/cosmicbaseballinit/domain/ScoreBoard.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/domain/ScoreBoard.java
@@ -11,28 +11,40 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 public class ScoreBoard {
+
     @Setter
     private Long id;
     private int score;
     private static final int COUNT_OF_BASES = 4;
     private final List<Integer> onBaseList = new ArrayList<>();
     private final AtBatResult atBatResult = new AtBatResult();
+    private boolean feverInning;
 
-    public ScoreBoard(List<Integer> baseNumbers ) {
+    public ScoreBoard(List<Integer> baseNumbers) {
         onBaseList.addAll(baseNumbers);
     }
-    
-    public void adjust(BattingResult result){
+
+    public void adjust(BattingResult result) {
         atBatResult.adjustAccordingToBattingResult(result);
-        if(atBatResult.getBatterStatus() == GO_TO_BASE){
+        if (atBatResult.getBatterStatus() == GO_TO_BASE) {
             adjustBaseAndScore();
         }
     }
+
     private void adjustBaseAndScore() {
         onBaseList.add(onBaseList.size() + 1);
+        if (isRunInFeverInning()) {
+            doScoringProcess();
+            return;
+        }
+
         if (isRun()) {
             doScoringProcess();
         }
+    }
+
+    private boolean isRunInFeverInning() {
+        return this.feverInning && onBaseList.get(onBaseList.size() - 1) == 3;
     }
 
     private void doScoringProcess() {
@@ -42,5 +54,9 @@ public class ScoreBoard {
 
     private boolean isRun() {
         return COUNT_OF_BASES == onBaseList.get(onBaseList.size() - 1);
+    }
+
+    public void startFeverInning() {
+        this.feverInning = true;
     }
 }
