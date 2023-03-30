a0f08b5a35ace028744ff8b3410b495043e8ad34
cohi <tkfka156@gmail.com>
2023-03-30 19:33:55 +0900

    :sparkles: feat: NewGameDto 생성

---
+++ src/main/java/com/hyunec/cosmicbaseballinit/dto/NewGameDto.java
@@ -0,0 +1,11 @@
+package com.hyunec.cosmicbaseballinit.dto;
+
+import com.hyunec.cosmicbaseballinit.contoller.InningType;
+import lombok.Data;
+
+@Data
+public class NewGameDto {
+
+    private InningType inningType;
+
+}
