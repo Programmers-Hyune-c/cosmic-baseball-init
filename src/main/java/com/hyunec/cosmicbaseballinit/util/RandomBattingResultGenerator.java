3f4415acb4848fa01e7b1d4e2ec5c4931a2cd702
cohi <tkfka156@gmail.com>
2023-03-30 19:38:46 +0900

    :sparkles: feat(RandomBattingResultGenerator): fever 이닝 안타 확률 2배 로직 구현

--- src/main/java/com/hyunec/cosmicbaseballinit/util/RandomBattingResultGenerator.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/util/RandomBattingResultGenerator.java
@@ -1,4 +1,4 @@
-package com.hyunec.cosmicbaseballinit.service;
+package com.hyunec.cosmicbaseballinit.util;
 
 import static com.hyunec.cosmicbaseballinit.domain.BattingResult.BALL;
 import static com.hyunec.cosmicbaseballinit.domain.BattingResult.DOUBLE_BALL;
@@ -20,7 +20,7 @@ public class RandomBattingResultGenerator {
     private static final List<BattingResult> BATTING_RESULT_LIST =
         List.of(STRIKE, BALL, HIT, DOUBLE_STRIKE, DOUBLE_BALL);
     private static final int BOUND = 100;
-    private static final int HIT_PERCENT = 40;
+    private static final int HIT_PERCENT = 50;
 
     public static BattingResult get(int percentage, BattingResult target) {
         if (isInPercentage(percentage)) {
@@ -30,12 +30,25 @@ public class RandomBattingResultGenerator {
         return isBullEyeResult(result) ? result.toBullEyeResult() : result;
     }
 
-    public static BattingResult getResultOnFever() {
-
-        if(isInPercentage(HIT_PERCENT)){
-           return HIT;
+    public static BattingResult getOnFever(int percentage, BattingResult target) {
+        if (target == HIT) {
+            return getResultOnHitPercentageDouble(percentage, target);
         }
-        BattingResult result = getBattingResultExcept(HIT);
+
+        if (isInPercentage(percentage)) {
+            return target;
+        }
+
+        BattingResult result = getFeverBattingResultExcept(target);
+        return isBullEyeResult(result) ? result.toBullEyeResult() : result;
+    }
+
+    private static BattingResult getResultOnHitPercentageDouble(int percentage,
+        BattingResult target) {
+        if (isInPercentage(percentage * 2)) {
+            return target;
+        }
+        BattingResult result = getBattingResultExcept(target);
         return isBullEyeResult(result) ? result.toBullEyeResult() : result;
     }
 
@@ -44,12 +57,28 @@ public class RandomBattingResultGenerator {
     }
 
     private static BattingResult getBattingResultExcept(BattingResult target) {
-        List<BattingResult> noTargetResultList = battingResultListExcept(target);
+        List<BattingResult> noTargetResultList =
+            battingResultListExcept(target, BATTING_RESULT_LIST);
         return noTargetResultList.get(RANDOM.nextInt(noTargetResultList.size()));
     }
 
-    public static List<BattingResult> battingResultListExcept(BattingResult target) {
-        return BATTING_RESULT_LIST.stream().filter(e -> e != target).collect(Collectors.toList());
+    private static BattingResult getFeverBattingResultExcept(BattingResult target) {
+        List<BattingResult> noTargetResultList =
+            battingResultListExcept(target, BATTING_RESULT_LIST);
+        return getFeverBattingResult(noTargetResultList);
+    }
+
+    private static BattingResult getFeverBattingResult(List<BattingResult> noTargetResultList) {
+        if (isInPercentage(HIT_PERCENT)) {
+            return HIT;
+        }
+        List<BattingResult> noHitBattingResults = battingResultListExcept(HIT, noTargetResultList);
+        return noHitBattingResults.get(RANDOM.nextInt(noHitBattingResults.size()));
+    }
+
+    public static List<BattingResult> battingResultListExcept(BattingResult target,
+        List<BattingResult> list) {
+        return list.stream().filter(e -> e != target).collect(Collectors.toList());
     }
 
     private static boolean isBullEyeResult(BattingResult result) {
@@ -61,6 +90,4 @@ public class RandomBattingResultGenerator {
     private static BattingResult getRandomResult() {
         return BattingResult.values()[RANDOM.nextInt(BATTING_RESULT_LIST.size())];
     }
-
-
 }
