5447c6879dae818845a3ff91183ad09f1b6eefdc
Jungeunkim-dev <svaha@ewhain.net>
2023-04-06 19:29:37 +0900

    :sparkles: feat : 타격 controller, service 구조 추가

---
+++ src/main/java/com/hyunec/cosmicbaseballinit/controller/BattingController.java
@@ -0,0 +1,7 @@
+package com.hyunec.cosmicbaseballinit.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BattingController {
+}
---
+++ src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
@@ -0,0 +1,7 @@
+package com.hyunec.cosmicbaseballinit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BattingService {
+}
