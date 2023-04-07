72ab2e2625eb9eb11b84b8b301eb4bc5f5910b9e
Jungeunkim-dev <svaha@ewhain.net>
2023-04-07 15:27:05 +0900

    :white_check_mark: test : 일반야구 기본 t1,t2 테스트 추가

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv1Test.java
@@ -1,18 +1,63 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
+import com.hyunec.cosmicbaseballinit.model.BattingResult;
+import com.hyunec.cosmicbaseballinit.service.BattingService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+
+@SpringBootTest
 class NormalBaseballLv1Test {
+
+    @Autowired
+    private BattingService battingService;
+
     @DisplayName("타격 결과는 모두 같은 확률을 가집니다.")
     @Test
     void t1() {
-        throw new RuntimeException("Not yet implemented");
+
+        double strikeCount = 0;
+        double ballCount = 0;
+        double hitCount = 0;
+
+        double caseSize = 150000;
+
+        for (double i = 0; i < caseSize; i++) {
+            BattingResult result = battingService.batting();
+
+            switch (result) {
+                case STRIKE:
+                    strikeCount += 1;
+                    break;
+                case BALL:
+                    ballCount += 1;
+                    break;
+                case HIT:
+                    hitCount += 1;
+                    break;
+            }
+        }
+
+        double strikeProbability = Math.round(strikeCount / caseSize * 10) / 10F;
+        double ballProbability = Math.round(ballCount / caseSize * 10) / 10F;
+        double hitProbability = Math.round(hitCount / caseSize * 10) / 10F;
+
+        Assertions.assertTrue(strikeProbability == ballProbability && ballProbability == hitProbability);
     }
 
     @DisplayName("타격 결과는 strike, ball, hit 입니다.")
     @Test
     void t2() {
-        throw new RuntimeException("Not yet implemented");
+
+        BattingResult result = battingService.batting();
+
+        boolean validResult = result.equals(BattingResult.HIT)
+                || result.equals(BattingResult.STRIKE)
+                || result.equals(BattingResult.BALL);
+
+        Assertions.assertTrue(validResult);
     }
 }
