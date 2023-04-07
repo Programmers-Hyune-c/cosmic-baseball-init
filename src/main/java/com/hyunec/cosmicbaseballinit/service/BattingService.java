de08af06f1804b5dff0d77376f66ffd1a6c41572
Jungeunkim-dev <svaha@ewhain.net>
2023-04-07 15:23:13 +0900

    :sparkles: feat : 동일한 확률로 타격 결과 결정하는 로직 추가

--- src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
@@ -1,7 +1,26 @@
 package com.hyunec.cosmicbaseballinit.service;
 
+import com.hyunec.cosmicbaseballinit.model.BattingResult;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class BattingService {
+
+    public BattingResult batting() {
+
+        Random random = new Random();
+
+        int value = random.nextInt(3);
+
+        switch (value) {
+            case 0:
+                return BattingResult.STRIKE;
+            case 1:
+                return BattingResult.BALL;
+            default:
+                return BattingResult.HIT;
+        }
+    }
 }
