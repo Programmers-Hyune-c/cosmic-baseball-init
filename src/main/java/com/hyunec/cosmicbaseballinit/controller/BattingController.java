3acbe057c1df03829c397b95e1789a7e2a1e37fd
Jungeunkim-dev <svaha@ewhain.net>
2023-04-07 15:24:36 +0900

    :sparkles: feat : 타격 결과 리턴 api 추가

--- src/main/java/com/hyunec/cosmicbaseballinit/controller/BattingController.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/controller/BattingController.java
@@ -1,7 +1,23 @@
 package com.hyunec.cosmicbaseballinit.controller;
 
+import com.hyunec.cosmicbaseballinit.model.BattingResult;
+import com.hyunec.cosmicbaseballinit.service.BattingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class BattingController {
+
+    private final BattingService battingService;
+
+    @GetMapping("/batting")
+    public ResponseEntity<BattingResult> getBattingResult() {
+        BattingResult result = battingService.batting();
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
