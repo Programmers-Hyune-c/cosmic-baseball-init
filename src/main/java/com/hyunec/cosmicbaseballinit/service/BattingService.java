ec51386e07edb845f4a4deba226dbf3d65df85ee
Jungeunkim-dev <svaha@ewhain.net>
2023-04-09 04:05:38 +0900

    :recycle: refactor : BattingService 클래스에서 randomUtils 위임받도록 수정

--- src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/service/BattingService.java
@@ -1,26 +1,17 @@
 package com.hyunec.cosmicbaseballinit.service;
 
+import com.hyunec.cosmicbaseballinit.common.RandomUtils;
 import com.hyunec.cosmicbaseballinit.model.BattingResult;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
-
 @Service
+@RequiredArgsConstructor
 public class BattingService {
 
-    private final Random random = new Random();
+    private final RandomUtils randomUtils;
 
     public BattingResult batting() {
-
-        int value = random.nextInt(3);
-
-        switch (value) {
-            case 0:
-                return BattingResult.STRIKE;
-            case 1:
-                return BattingResult.BALL;
-            default:
-                return BattingResult.HIT;
-        }
+        return randomUtils.getRandomBattingResult();
     }
 }
