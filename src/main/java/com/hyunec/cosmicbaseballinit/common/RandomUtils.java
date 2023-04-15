60e741c6cf8dbaddb1c0c2c1133e39edaffc6871
Jungeunkim-dev <svaha@ewhain.net>
2023-04-15 15:12:45 +0900

    :sparkles: feat : 랜덤 타격 결과 생성에 double strike, double ball 케이스 추가

--- src/main/java/com/hyunec/cosmicbaseballinit/common/RandomUtils.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/common/RandomUtils.java
@@ -13,13 +13,17 @@ public class RandomUtils {
     private static final Random RANDOM = new Random();
 
     public static BattingResult getRandomBattingResult() {
-        int value = RANDOM.nextInt(3);
+        int value = RANDOM.nextInt(5);
 
         switch (value) {
             case 0:
                 return STRIKE;
             case 1:
                 return BALL;
+            case 2:
+                return DOUBLE_STRIKE;
+            case 3:
+                return DOUBLE_BALL;
             default:
                 return HIT;
         }
