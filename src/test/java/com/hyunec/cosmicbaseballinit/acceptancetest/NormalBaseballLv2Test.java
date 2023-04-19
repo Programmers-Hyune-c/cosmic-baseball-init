fa7a8cf2563ef9fea02fb4d319a3a51a34f60b2e
Jungeunkim-dev <svaha@ewhain.net>
2023-04-19 22:24:39 +0900

    :white_check_mark: test : 일반야구 Lv2. strike, ball 다이나믹 테스트로 구조 수정

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv2Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv2Test.java
@@ -1,61 +1,61 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.stream.Stream;
+
+@SpringBootTest
 class NormalBaseballLv2Test {
-    @DisplayName("타석 상태를 표현할 수 있습니다.")
-    @Test
-    void t1() {
-        throw new RuntimeException("Not yet implemented");
+
+    @DisplayName("strike 타석 결과와 관련된 테스트를 진행합니다.")
+    @TestFactory
+    Stream<DynamicTest> normalBaseBallLv2TestStrikeCollection() {
+        return Stream.of(
+                DynamicTest.dynamicTest("새로운 타석이 시작되면 타석 상태를 초기화 합니다.", () -> {
+
+                }),
+                DynamicTest.dynamicTest("0 strike 상태의 타석에서 타격 결과가 strike 이면 타석 결과는 진행 중 입니다.", () -> {
+
+                }),
+                DynamicTest.dynamicTest("진행 중인 타석이 있는 상태에서 새로운 타석을 진행할 수 없습니다.", () -> {
+
+                }),
+                DynamicTest.dynamicTest("1 strike 상태의 타석에서 타격 결과가 double_strike 이면 타석 결과는 out 입니다.", () -> {
+
+                }),
+                DynamicTest.dynamicTest("2 strike 상태의 타석에서 타격 결과가 strike 이면 타석 결과는 out 입니다.", () -> {
+
+                })
+        );
     }
 
-    @DisplayName("0 strike 상태의 타석에서 타격 결과가 strike 이면 타석 결과는 진행 중 입니다.")
-    @Test
-    void t2() {
-        throw new RuntimeException("Not yet implemented");
-    }
+    @DisplayName("ball 타석 결과와 관련된 테스트를 진행합니다.")
+    @TestFactory
+    Stream<DynamicTest> normalBaseBallLv2TestBallCollection() {
+        return Stream.of(
+                DynamicTest.dynamicTest("새로운 타석이 시작되면 타석 상태를 초기화 합니다.", () -> {
 
-    @DisplayName("2 strike 상태의 타석에서 타격 결과가 strike 이면 타석 결과는 out 입니다.")
-    @Test
-    void t3() {
-        throw new RuntimeException("Not yet implemented");
-    }
+                }),
+                DynamicTest.dynamicTest("0 ball 상태의 타석에서 타격 결과가 ball 이면 타석 결과는 진행 중 입니다.", () -> {
 
-    @DisplayName("0 strike 상태의 타석에서 타격 결과가 double_strike 이면 타석 결과는 진행 중 입니다.")
-    @Test
-    void t4() {
-        throw new RuntimeException("Not yet implemented");
-    }
+                }),
+                DynamicTest.dynamicTest("진행 중인 타석이 있는 상태에서 새로운 타석을 진행할 수 없습니다.", () -> {
 
-    @DisplayName("1 strike 상태의 타석에서 타격 결과가 double_strike 이면 타석 결과는 out 입니다.")
-    @Test
-    void t5() {
-        throw new RuntimeException("Not yet implemented");
-    }
+                }),
+                DynamicTest.dynamicTest("0 ball 상태의 타석에서 타격 결과가 double_ball 이면 타석 결과는 진행 중 입니다.", () -> {
 
-    @DisplayName("0 ball 상태의 타석에서 타격 결과가 ball 이면 타석 결과는 진행 중 입니다.")
-    @Test
-    void t6() {
-        throw new RuntimeException("Not yet implemented");
-    }
+                }),
+                DynamicTest.dynamicTest("2 ball 상태의 타석에서 타격 결과가 double_ball 이면 타석 결과는 four_ball 으로 진루 입니다.", () -> {
 
-    @DisplayName("3 ball 상태의 타석에서 타격 결과가 ball 이면 타석 결과는 four_ball 으로 진루 입니다.")
-    @Test
-    void t7() {
-        throw new RuntimeException("Not yet implemented");
-    }
+                }),
+                DynamicTest.dynamicTest("3 ball 상태의 타석에서 타격 결과가 ball 이면 타석 결과는 four_ball 으로 진루 입니다.", () -> {
 
-    @DisplayName("0 ball 상태의 타석에서 타격 결과가 double_ball 이면 타석 결과는 진행 중 입니다.")
-    @Test
-    void t8() {
-        throw new RuntimeException("Not yet implemented");
-    }
-
-    @DisplayName("2 ball 상태의 타석에서 타격 결과가 double_ball 이면 타석 결과는 four_ball 으로 진루 입니다.")
-    @Test
-    void t9() {
-        throw new RuntimeException("Not yet implemented");
+                })
+        );
     }
 
     @DisplayName("타석 결과가 hit 이면 타석 결과는 hit 로 진루 입니다.")
