4c181a527027d3a0b29cb0dc8499223dac7d5c90
Jungeunkim-dev <svaha@ewhain.net>
2023-04-19 22:53:18 +0900

    :white_check_mark: test : strike 다이나믹 테스트 추가

--- src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv2Test.java
+++ src/test/java/com/hyunec/cosmicbaseballinit/acceptancetest/NormalBaseballLv2Test.java
@@ -1,5 +1,7 @@
 package com.hyunec.cosmicbaseballinit.acceptancetest;
 
+import com.hyunec.cosmicbaseballinit.model.BatterStatus;
+import com.hyunec.cosmicbaseballinit.model.PlateStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
@@ -8,27 +10,35 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.stream.Stream;
 
+import static com.hyunec.cosmicbaseballinit.model.BattingResult.DOUBLE_STRIKE;
+import static com.hyunec.cosmicbaseballinit.model.BattingResult.STRIKE;
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class NormalBaseballLv2Test {
 
     @DisplayName("strike 타석 결과와 관련된 테스트를 진행합니다.")
     @TestFactory
     Stream<DynamicTest> normalBaseBallLv2TestStrikeCollection() {
+
+        PlateStatus plateStatus = new PlateStatus();
+
         return Stream.of(
                 DynamicTest.dynamicTest("새로운 타석이 시작되면 타석 상태를 초기화 합니다.", () -> {
-
+                    assertThat(plateStatus.getBallCount()).isZero();
+                    assertThat(plateStatus.getStrikeCount()).isZero();
+                    assertThat(plateStatus.getHitCount()).isZero();
+                    assertThat(plateStatus.getBatterStatus()).isEqualTo(BatterStatus.STAY);
                 }),
                 DynamicTest.dynamicTest("0 strike 상태의 타석에서 타격 결과가 strike 이면 타석 결과는 진행 중 입니다.", () -> {
-
-                }),
-                DynamicTest.dynamicTest("진행 중인 타석이 있는 상태에서 새로운 타석을 진행할 수 없습니다.", () -> {
-
+                    plateStatus.updateBatterResult(STRIKE);
+                    BatterStatus batterStatus = plateStatus.getBatterStatus();
+                    assertThat(batterStatus).isEqualTo(BatterStatus.STAY);
                 }),
                 DynamicTest.dynamicTest("1 strike 상태의 타석에서 타격 결과가 double_strike 이면 타석 결과는 out 입니다.", () -> {
-
-                }),
-                DynamicTest.dynamicTest("2 strike 상태의 타석에서 타격 결과가 strike 이면 타석 결과는 out 입니다.", () -> {
-
+                    plateStatus.updateBatterResult(DOUBLE_STRIKE);
+                    BatterStatus batterStatus = plateStatus.getBatterStatus();
+                    assertThat(batterStatus).isEqualTo(BatterStatus.OUT);
                 })
         );
     }
