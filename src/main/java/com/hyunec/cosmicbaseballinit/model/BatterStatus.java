e691db7d0c9f9e2ff2175a007c4d1e8a756a74ad
Jungeunkim-dev <svaha@ewhain.net>
2023-04-24 20:46:22 +0900

    :recycle: refactor : BatterStatus enum에 description field 추가

--- src/main/java/com/hyunec/cosmicbaseballinit/model/BatterStatus.java
+++ src/main/java/com/hyunec/cosmicbaseballinit/model/BatterStatus.java
@@ -1,11 +1,14 @@
 package com.hyunec.cosmicbaseballinit.model;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum BatterStatus {
-    STAY,
-    ADVANCE,
-    OUT
+    STAY("진루하지 않음"),
+    ADVANCE("진루"),
+    OUT("아웃");
 
+    private final String description;
 }
